d7461ac8adfe88f7de4831af8574a536be433923
mjmealey <markjmealey@gmail.com>
2023-06-09 19:41:12 -0500

    content paths updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html", "./src/multiplayer.html"],
+  content: ["./src/index.html", "./src/multiplayer.html", "./src/app.js"],
   theme: {
     extend: {},
   },
