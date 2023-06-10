fca53da8f3ff1913cbcfba9182f76b832df87d70
mjmealey <markjmealey@gmail.com>
2023-06-09 23:40:21 -0500

    text content for the start of the chess game added with nested arrays

--- src/modules/vsComputer.js
+++ src/modules/vsComputer.js
@@ -5,87 +5,89 @@ const vsComputer = () => {
     rowId: "rows",
     gridElementsClass: "gridElements",
   };
-  const gridCellIds = {
+
+  const gridCellIds = [
     //row one
-    rowOneCellOne: "cell-0-0",
-    rowOneCellTwo: "cell-0-1",
-    rowOneCellThree: "cell-0-2",
-    rowOneCellFour: "cell-0-3",
-    rowOneCellFive: "cell-0-4",
-    rowOneCellSix: "cell-0-5",
-    rowOneCellSeven: "cell-0-6",
-    rowOneCellEight: "cell-0-7",
+    "cell-0-0",
+    "cell-0-1",
+    "cell-0-2",
+    "cell-0-3",
+    "cell-0-4",
+    "cell-0-5",
+    "cell-0-6",
+    "cell-0-7",
 
     //row two
-    rowTwoCellOne: "cell-1-0",
-    rowTwoCellTwo: "cell-1-1",
-    rowTwoCellThree: "cell-1-2",
-    rowTwoCellFour: "cell-1-3",
-    rowTwoCellFive: "cell-1-4",
-    rowTwoCellSix: "cell-1-5",
-    rowTwoCellSeven: "cell-1-6",
-    rowTwoCellEight: "cell-1-7",
+    "cell-1-0",
+    "cell-1-1",
+    "cell-1-2",
+    "cell-1-3",
+    "cell-1-4",
+    "cell-1-5",
+    "cell-1-6",
+    "cell-1-7",
 
     //row three
-    rowThreeCellOne: "cell-2-0",
-    rowThreeCellTwo: "cell-2-1",
-    rowThreeCellThree: "cell-2-2",
-    rowThreeCellFour: "cell-2-3",
-    rowThreeCellFive: "cell-2-4",
-    rowThreeCellSix: "cell-2-5",
-    rowThreeCellSeven: "cell-2-6",
-    rowThreeCellEight: "cell-2-7",
+    "cell-2-0",
+    "cell-2-1",
+    "cell-2-2",
+    "cell-2-3",
+    "cell-2-4",
+    "cell-2-5",
+    "cell-2-6",
+    "cell-2-7",
 
     //row four
-    rowFourCellOne: "cell-3-0",
-    rowFourCellTwo: "cell-3-1",
-    rowFourCellThree: "cell-3-2",
-    rowFourCellFour: "cell-3-3",
-    rowFourCellFive: "cell-3-4",
-    rowFourCellSix: "cell-3-5",
-    rowFourCellSeven: "cell-3-6",
-    rowFourCellEight: "cell-3-7",
+    "cell-3-0",
+    "cell-3-1",
+    "cell-3-2",
+    "cell-3-3",
+    "cell-3-4",
+    "cell-3-5",
+    "cell-3-6",
+    "cell-3-7",
 
     //row five
-    rowFiveCellOne: "cell-4-0",
-    rowFiveCellTwo: "cell-4-1",
-    rowFiveCellThree: "cell-4-2",
-    rowFiveCellFour: "cell-4-3",
-    rowFiveCellFive: "cell-4-4",
-    rowFiveCellSix: "cell-4-5",
-    rowFiveCellSeven: "cell-4-6",
-    rowFiveCellEight: "cell-4-7",
+    "cell-4-0",
+    "cell-4-1",
+    "cell-4-2",
+    "cell-4-3",
+    "cell-4-4",
+    "cell-4-5",
+    "cell-4-6",
+    "cell-4-7",
 
-    //row six
-    rowSixCellOne: "cell-5-0",
-    rowSixCellTwo: "cell-5-1",
-    rowSixCellThree: "cell-5-2",
-    rowSixCellFour: "cell-5-3",
-    rowSixCellFive: "cell-5-4",
-    rowSixCellSix: "cell-5-5",
-    rowSixCellSeven: "cell-5-6",
-    rowSixCellEight: "cell-5-7",
+    //row six,
+    "cell-5-0",
+    "cell-5-1",
+    "cell-5-2",
+    "cell-5-3",
+    "cell-5-4",
+    "cell-5-5",
+    "cell-5-6",
+    "cell-5-7",
 
     //row seven
-    rowSevenCellOne: "cell-6-0",
-    rowSevenCellTwo: "cell-6-1",
-    rowSevenCellThree: "cell-6-2",
-    rowSevenCellFour: "cell-6-3",
-    rowSevenCellFive: "cell-6-4",
-    rowSevenCellSix: "cell-6-5",
-    rowSevenCellSeven: "cell-6-6",
-    rowSevenCellEight: "cell-6-7",
+    "cell-6-0",
+    "cell-6-1",
+    "cell-6-2",
+    "cell-6-3",
+    "cell-6-4",
+    "cell-6-5",
+    "cell-6-6",
+    "cell-6-7",
 
     //row eight
-    rowEightCellOne: "cell-7-0",
-    rowEightCellTwo: "cell-7-1",
-    rowEightCellThree: "cell-7-2",
-    rowEightCellFour: "cell-7-3",
-    rowEightCellFive: "cell-7-4",
-    rowEightCellSix: "cell-7-5",
-    rowEightCellSeven: "cell-7-6",
-    rowEightCellEight: "cell-7-7",
-  };
+
+    "cell-7-0",
+    "cell-7-1",
+    "cell-7-2",
+    "cell-7-3",
+    "cell-7-4",
+    "cell-7-5",
+    "cell-7-6",
+    "cell-7-7",
+  ];
 
   const computerGrid = () => {
     console.log(gridElements.cellId);
@@ -109,11 +111,62 @@ const vsComputer = () => {
   };
 
   const gridCellFunctionality = () => {
+    const mappedCellIds = gridCellIds.map((id) => document.getElementById(id));
+    const chessMovePlaceHolder = {
+      pawn: "Pawn",
+      rook: "Rook",
+      queen: "Queen",
+      king: "King",
+      bishop: "Bishop",
+      knight: "Knight",
+    };
 
-        const gridCellTextContent = {
-            
-        }
+    const emptyCell = "";
+    let filledCell = chessMovePlaceHolder.pawn;
 
+    const chessPiecePositions = [
+      //top two rows
+      [mappedCellIds[0], chessMovePlaceHolder.rook],
+      [mappedCellIds[1], chessMovePlaceHolder.pawn],
+      [mappedCellIds[8], chessMovePlaceHolder.knight],
+      [mappedCellIds[9], chessMovePlaceHolder.pawn],
+      [mappedCellIds[16], chessMovePlaceHolder.bishop],
+      [mappedCellIds[17], chessMovePlaceHolder.pawn],
+      [mappedCellIds[24], chessMovePlaceHolder.queen],
+      [mappedCellIds[25], chessMovePlaceHolder.pawn],
+      [mappedCellIds[32], chessMovePlaceHolder.king],
+      [mappedCellIds[33], chessMovePlaceHolder.pawn],
+      [mappedCellIds[40], chessMovePlaceHolder.bishop],
+      [mappedCellIds[41], chessMovePlaceHolder.pawn],
+      [mappedCellIds[48], chessMovePlaceHolder.knight],
+      [mappedCellIds[49], chessMovePlaceHolder.pawn],
+      [mappedCellIds[56], chessMovePlaceHolder.rook],
+      [mappedCellIds[57], chessMovePlaceHolder.pawn],
+
+      //bottom two rows
+      [mappedCellIds[6], chessMovePlaceHolder.pawn],
+      [mappedCellIds[7], chessMovePlaceHolder.rook],
+      [mappedCellIds[14], chessMovePlaceHolder.pawn],
+      [mappedCellIds[15], chessMovePlaceHolder.knight],
+      [mappedCellIds[22], chessMovePlaceHolder.pawn],
+      [mappedCellIds[23], chessMovePlaceHolder.bishop],
+      [mappedCellIds[30], chessMovePlaceHolder.pawn],
+      [mappedCellIds[31], chessMovePlaceHolder.queen],
+      [mappedCellIds[38], chessMovePlaceHolder.pawn],
+      [mappedCellIds[39], chessMovePlaceHolder.king],
+      [mappedCellIds[46], chessMovePlaceHolder.pawn],
+      [mappedCellIds[47], chessMovePlaceHolder.bishop],
+      [mappedCellIds[54], chessMovePlaceHolder.pawn],
+      [mappedCellIds[55], chessMovePlaceHolder.knight],
+      [mappedCellIds[62], chessMovePlaceHolder.pawn],
+      [mappedCellIds[63], chessMovePlaceHolder.rook],
+    ];
+
+    const arrangedPositions = chessPiecePositions.forEach(
+      ([mappedCellIds, chessMovePlaceHolder]) => {
+        mappedCellIds.textContent = chessMovePlaceHolder;
+      }
+    );
 
     return { gridCellFunctionality };
   };
