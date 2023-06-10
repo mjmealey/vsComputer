02388eac2295abdb74f4cbc855be4eee87f1a536
mjmealey <markjmealey@gmail.com>
2023-06-09 19:33:51 -0500

    import of computer module file and link function created for main page buttons

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import vsComputer from "./modules/vsComputer.js"
+
+const newVsComputer = vsComputer()
+
+newVsComputer.computerGrid(8,8)
+
+const mainPageLinks = () => {
+  const buttonIds = ["pvpLink", "vsComputerLink", "rulesLink"];
+  const mapButtonIds = buttonIds.map((id) => document.getElementById(id));
+  document.addEventListener("click", (e) => {
+    if (e.target.id === mapButtonIds[0]) {
+      window.location.href = "";
+    } else if (e.target.id === mapButtonIds[1]) {
+      window.location.href = "";
+    } else if (e.target.id === mapButtonIds[2]) {
+      window.location.href = "";
+    }
+  });
+};
+
+const mainPageLinksCall = mainPageLinks()
