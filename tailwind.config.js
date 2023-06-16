9ab6a30fdb4466023913dcd08bc5de74a0744f3e
mjmealey <markjmealey@gmail.com>
2023-06-16 16:44:28 -0500

    files removed, modified and changed

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/index.html", "./src/multiplayer.html", "./src/app.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
