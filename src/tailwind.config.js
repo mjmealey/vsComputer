1e58df82cb675742b0e615f22cb9f42eb6a515dc
mjmealey <markjmealey@gmail.com>
2023-06-16 16:41:53 -0500

    files removed, changed and modified due to removing typescript

---
+++ src/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html", "./src/multiplayer.html", "./src/app.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
