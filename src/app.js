3d5479c2a34ec92a8bcfd00b4c391dc317912731
mjmealey <markjmealey@gmail.com>
2023-06-16 16:38:16 -0500

    app.js file edited to just contain the import and function invocation, will be adding more later

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const vsComputer_js_1 = __importDefault(require("./modules/vsComputer/vsComputer.js"));
-(0, vsComputer_js_1.default)();
-
-import vsComputer from "./modules/vsComputer/vsComputer.js"
+import vsComputer from "./Modules/vsComputer/vsComputer.js"
 
 vsComputer()
