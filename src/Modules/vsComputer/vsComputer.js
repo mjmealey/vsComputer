ac2bcba57c195f4a1789f545d11269a7f0568880
mjmealey <markjmealey@gmail.com>
2023-06-16 21:26:05 -0500

    chess move functions imported into and invoked in  main vsComputer file

--- src/Modules/vsComputer/vsComputer.js
+++ src/Modules/vsComputer/vsComputer.js
@@ -1,9 +1,17 @@
+
 import chessBoard from "./ChessBoard.js"
 
 const vsComputer = () => {
   const newChessBoard = chessBoard()
+  newChessBoard.createChessBoard()
   newChessBoard.chessGame()
   newChessBoard.cellColors()
+  newChessBoard.rookMoves()
+  newChessBoard.knightMoves()
+  newChessBoard.pawnMoves()
+  newChessBoard.queenMoves()
+  newChessBoard.bishopMoves()
+  newChessBoard.kingMoves()
  
 }
 
