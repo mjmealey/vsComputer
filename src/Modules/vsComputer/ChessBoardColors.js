8f898f610a83fbe336e0c8b948b6079a4b4ecabc
mjmealey <markjmealey@gmail.com>
2023-06-24 17:57:06 -0500

    chess board colors functions put into a module to keep the code organized, simple to read, and reusable

---
+++ src/Modules/vsComputer/ChessBoardColors.js
@@ -0,0 +1,207 @@
+import { getCellIds } from "./ChessPieceAssignments.js";
+const ChessBoardColors = () => {
+    const cellColors = (mappedCellIds) => {
+    mappedCellIds = getCellIds.map((id) => document.getElementById(id));
+
+    const gridColors = {
+      //default grid
+      bisque: "bisque",
+      brown: "#4b2424",
+      //woodland grid
+      green: "green",
+      woodBrown: "#8B4513",
+      //checker grid
+      red: "red",
+      black: "black",
+      //blue sky grid
+      blue: "blue",
+      white: "white",
+      //fun kingdom grid
+      purple: "purple",
+      pink: "pink",
+      //classic grid
+      classicBlack: "black",
+      classicWhite: "white",
+      //kingdom grid
+      yellow: "yellow",
+      blue: "blue",
+      //fire and ice grid
+      fireRed: "crimson",
+      iceBlue: "lightblue",
+    };
+
+    const colorSelectAttributes = {
+      //ids
+      selectId: "colorSelect",
+
+      //values
+      selectColor: "selectColor",
+      default: "default",
+      woodland: "woodland",
+      checker: "checker",
+      blueSky: "blueSky",
+      funKingdom: "funKingdom",
+      classic: "classic",
+      kingdom: "kingdom",
+      fireAndIce: "fireAndIce",
+    };
+
+    let primaryColor = [
+      0, 2, 4, 6, 9, 11, 13, 15, 16, 18, 20, 22, 25, 27, 29, 31, 32, 34, 36, 38,
+      41, 43, 45, 47, 48, 50, 52, 54, 57, 59, 61, 63,
+    ];
+    let secondaryColor = [
+      1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23, 24, 26, 28, 30, 33, 35, 37, 39,
+      40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62,
+    ];
+
+    for (
+      let defaultColorIndex = 0;
+      defaultColorIndex < getCellIds.length;
+      defaultColorIndex++
+    ) {
+      if (primaryColor.includes(defaultColorIndex)) {
+        mappedCellIds[defaultColorIndex].style.backgroundColor =
+          gridColors.bisque;
+        mappedCellIds[defaultColorIndex].style.borderColor = gridColors.bisque;
+      } else if (secondaryColor.includes(defaultColorIndex)) {
+        mappedCellIds[defaultColorIndex].style.backgroundColor =
+          gridColors.brown;
+        mappedCellIds[defaultColorIndex].style.borderColor = gridColors.brown;
+      }
+    }
+
+    colorSelect.addEventListener("input", (e) => {
+      const selectedColor = e.target.id;
+      if (selectedColor === colorSelectAttributes.selectId) {
+        switch (true) {
+          case colorSelect.value === colorSelectAttributes.default:
+            gridCellIds.forEach((_, defaultColors) => {
+              if (primaryColor.includes(defaultColors)) {
+                mappedCellIds[defaultColors].style.backgroundColor =
+                  gridColors.bisque;
+                mappedCellIds[defaultColors].style.borderColor =
+                  gridColors.bisque;
+              } else if (secondaryColor.includes(defaultColors)) {
+                mappedCellIds[defaultColors].style.backgroundColor =
+                  gridColors.brown;
+                mappedCellIds[defaultColors].style.borderColor =
+                  gridColors.brown;
+              }
+            });
+            break;
+          case colorSelect.value === colorSelectAttributes.woodland:
+            gridCellIds.forEach((_, woodlandColorIndex) => {
+              if (primaryColor.includes(woodlandColorIndex)) {
+                mappedCellIds[woodlandColorIndex].style.backgroundColor =
+                  gridColors.green;
+                mappedCellIds[woodlandColorIndex].style.borderColor =
+                  gridColors.green;
+              } else if (secondaryColor.includes(woodlandColorIndex)) {
+                mappedCellIds[woodlandColorIndex].style.backgroundColor =
+                  gridColors.woodBrown;
+                mappedCellIds[woodlandColorIndex].style.borderColor =
+                  gridColors.woodBrown;
+              }
+            });
+            break;
+          case colorSelect.value === colorSelectAttributes.checker:
+            gridCellIds.forEach((_, checkerColorIndex) => {
+              if (primaryColor.includes(checkerColorIndex)) {
+                mappedCellIds[checkerColorIndex].style.backgroundColor =
+                  gridColors.red;
+                mappedCellIds[checkerColorIndex].style.borderColor =
+                  gridColors.red;
+              } else if (secondaryColor.includes(checkerColorIndex)) {
+                mappedCellIds[checkerColorIndex].style.backgroundColor =
+                  gridColors.black;
+                mappedCellIds[checkerColorIndex].style.borderColor =
+                  gridColors.black;
+              }
+            });
+            break;
+          case colorSelect.value === colorSelectAttributes.blueSky:
+            gridCellIds.forEach((_, blueSkyColorIndex) => {
+              if (primaryColor.includes(blueSkyColorIndex)) {
+                mappedCellIds[blueSkyColorIndex].style.backgroundColor =
+                  gridColors.blue;
+                mappedCellIds[blueSkyColorIndex].style.borderColor =
+                  gridColors.blue;
+              } else if (secondaryColor.includes(blueSkyColorIndex)) {
+                mappedCellIds[blueSkyColorIndex].style.backgroundColor =
+                  gridColors.white;
+                mappedCellIds[blueSkyColorIndex].style.borderColor =
+                  gridColors.white;
+              }
+            });
+            break;
+          case colorSelect.value === colorSelectAttributes.funKingdom:
+            gridCellIds.forEach((_, funKingdomColorIndex) => {
+              if (primaryColor.includes(funKingdomColorIndex)) {
+                mappedCellIds[funKingdomColorIndex].style.backgroundColor =
+                  gridColors.purple;
+                mappedCellIds[funKingdomColorIndex].style.borderColor =
+                  gridColors.purple;
+              } else if (secondaryColor.includes(funKingdomColorIndex)) {
+                mappedCellIds[funKingdomColorIndex].style.backgroundColor =
+                  gridColors.pink;
+                mappedCellIds[funKingdomColorIndex].style.borderColor =
+                  gridColors.pink;
+              }
+            });
+            break;
+          case colorSelect.value === colorSelectAttributes.classic:
+            gridCellIds.forEach((_, classicColorIndex) => {
+              if (primaryColor.includes(classicColorIndex)) {
+                mappedCellIds[classicColorIndex].style.backgroundColor =
+                  gridColors.classicWhite;
+                mappedCellIds[classicColorIndex].style.borderColor =
+                  gridColors.classicWhite;
+              } else if (secondaryColor.includes(classicColorIndex)) {
+                mappedCellIds[classicColorIndex].style.backgroundColor =
+                  gridColors.classicBlack;
+                mappedCellIds[classicColorIndex].style.borderColor =
+                  gridColors.classicBlack;
+              }
+            });
+            break;
+          case colorSelect.value === colorSelectAttributes.kingdom:
+            gridCellIds.forEach((_, kingdomColorIndex) => {
+              if (primaryColor.includes(kingdomColorIndex)) {
+                mappedCellIds[kingdomColorIndex].style.backgroundColor =
+                  gridColors.blue;
+                mappedCellIds[kingdomColorIndex].style.borderColor =
+                  gridColors.blue;
+              } else if (secondaryColor.includes(kingdomColorIndex)) {
+                mappedCellIds[kingdomColorIndex].style.backgroundColor =
+                  gridColors.yellow;
+                mappedCellIds[kingdomColorIndex].style.borderColor =
+                  gridColors.yellow;
+              }
+            });
+            break;
+          case colorSelect.value === colorSelectAttributes.fireAndIce:
+            gridCellIds.forEach((_, fireAndIceColorIndex) => {
+              if (primaryColor.includes(fireAndIceColorIndex)) {
+                mappedCellIds[fireAndIceColorIndex].style.backgroundColor =
+                  gridColors.iceBlue;
+                mappedCellIds[fireAndIceColorIndex].style.borderColor =
+                  gridColors.iceBlue;
+              } else if (secondaryColor.includes(fireAndIceColorIndex)) {
+                mappedCellIds[fireAndIceColorIndex].style.backgroundColor =
+                  gridColors.fireRed;
+                mappedCellIds[fireAndIceColorIndex].style.borderColor =
+                  gridColors.fireRed;
+              }
+            });
+            break;
+        }
+      }
+    });
+    return { cellColors };
+  };
+  return {cellColors}
+}
+
+
+  export default ChessBoardColors
