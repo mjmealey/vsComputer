6023301c97926f109954ea5866df4e3e09e86cac
mjmealey <markjmealey@gmail.com>
2023-06-24 21:23:01 -0500

    all objects separated into their own files for easier use

---
+++ src/Modules/vsComputer/assignmentObjects.js
@@ -0,0 +1,156 @@
+const PAWN_ASSIGNMENTS = {
+  //starting pawn positions
+  pawnCell1: {
+    pieceName: "Pawn",
+    pieceNameOne: "Pawn",
+    pieceNameTwo: "Pawn",
+    pieceNameThree: "Pawn",
+    pieceNameFour: "Pawn",
+    pieceNameFive: "Pawn",
+    pawnCellNumOne: 1,
+    pawnCellNumTwo: 2,
+    pawnCellNumThree: 3,
+    pawnCellNumFour: 4,
+    pawnCellNumFive: 5,
+  },
+  pawnCell6: {
+    pieceName: "Pawn",
+    pawnCellNum: 2,
+  },
+  pawnCell9: {
+    pieceName: "Pawn",
+    pawnCellNum: 9,
+  },
+  pawnCell14: {
+    pieceName: "Pawn",
+    pawnCellNum: 14,
+  },
+  pawnCell17: {
+    pieceName: "Pawn",
+    pawnCellNum: 17,
+  },
+  pawnCell22: {
+    pieceName: "Pawn",
+    pawnCellNum: 22,
+  },
+  pawnCell25: {
+    pieceName: "Pawn",
+    pawnCellNum: 25,
+  },
+  pawnCell30: {
+    pieceName: "Pawn",
+    pawnCellNum: 30,
+  },
+  pawnCell33: {
+    pieceName: "Pawn",
+    pawnCellNum: 33,
+  },
+  pawnCell38: {
+    pieceName: "Pawn",
+    pawnCellNum: 38,
+  },
+  pawnCell41: {
+    pieceName: "Pawn",
+    pawnCellNum: 41,
+  },
+  pawnCell46: {
+    pieceName: "Pawn",
+    pawnCellNum: 46,
+  },
+  pawnCell49: {
+    pieceName: "Pawn",
+    pawnCellNum: 49,
+  },
+  pawnCell54: {
+    pieceName: "Pawn",
+    pawnCellNum: 54,
+  },
+  pawnCell57: {
+    pieceName: "Pawn",
+    pawnCellNum: 57,
+  },
+  pawnCell62: {
+    pieceName: "Pawn",
+    pawnCellNum: 62,
+  },
+};
+
+const ROOK_ASSIGNMENTS = {
+  //starting rook positions
+  rookCell0: "Rook",
+  rookCell7: "Rook",
+  rookCell56: "Rook",
+  rookCell63: "Rook",
+};
+
+const KNIGHT_ASSIGNMENTS = {
+  //starting knight positions
+  knightCell8: "Knight",
+  knightCell15: "Knight",
+  knightCell48: "Knight",
+  knightCell55: "Knight",
+};
+
+const BISHOP_ASSIGNMENTS = {
+  //starting bishop positions
+  bishopCell16: "Bishop",
+  bishopCell23: "Bishop",
+  bishopCell40: "Bishop",
+  bishopCell47: "Bishop",
+};
+
+const QUEEN_ASSIGNMENTS = {
+  //starting queen positions
+  queenCell24: "Queen",
+  queenCell31: "Queen",
+};
+
+const KING_ASSIGNMENTS = {
+  //starting king positions
+  kingCell32: "King",
+  kingCell39: "King",
+};
+
+const EMPTY_CELL_ASSIGNMENTS = {
+  //empty at start
+  emptyCell2: "",
+  emptyCell3: "",
+  emptyCell4: "",
+  emptyCell5: "",
+  emptyCell10: "",
+  emptyCell11: "",
+  emptyCell12: "",
+  emptyCell13: "",
+  emptyCell18: "",
+  emptyCell19: "",
+  emptyCell20: "",
+  emptyCell21: "",
+  emptyCell26: "",
+  emptyCell27: "",
+  emptyCell28: "",
+  emptyCell29: "",
+  emptyCell34: "",
+  emptyCell35: "",
+  emptyCell36: "",
+  emptyCell37: "",
+  emptyCell42: "",
+  emptyCell43: "",
+  emptyCell44: "",
+  emptyCell45: "",
+  emptyCell50: "",
+  emptyCell51: "",
+  emptyCell52: "",
+  emptyCell53: "",
+  emptyCell58: "",
+  emptyCell59: "",
+  emptyCell60: "",
+  emptyCell61: "",
+};
+
+export const pawnAssignmentObject = PAWN_ASSIGNMENTS;
+export const rookAssignmentObject = ROOK_ASSIGNMENTS;
+export const knightAssignmentObject = KNIGHT_ASSIGNMENTS;
+export const bishopAssignmentObject = BISHOP_ASSIGNMENTS;
+export const queenAssignmentObject = QUEEN_ASSIGNMENTS;
+export const kingAssignmentObject = KING_ASSIGNMENTS;
+export const emptyCellAssignments = EMPTY_CELL_ASSIGNMENTS;
---
+++ src/Modules/vsComputer/chessColorObjects.js
@@ -0,0 +1,45 @@
+const gridColors = {
+    //default grid
+    bisque: "bisque",
+    brown: "#4b2424",
+    //woodland grid
+    green: "green",
+    woodBrown: "#8B4513",
+    //checker grid
+    red: "red",
+    black: "black",
+    //blue sky grid
+    blue: "blue",
+    white: "white",
+    //fun kingdom grid
+    purple: "purple",
+    pink: "pink",
+    //classic grid
+    classicBlack: "black",
+    classicWhite: "white",
+    //kingdom grid
+    yellow: "yellow",
+    blue: "blue",
+    //fire and ice grid
+    fireRed: "crimson",
+    iceBlue: "lightblue",
+  };
+
+  const colorSelectAttributes = {
+    //ids
+    selectId: "colorSelect",
+
+    //values
+    selectColor: "selectColor",
+    default: "default",
+    woodland: "woodland",
+    checker: "checker",
+    blueSky: "blueSky",
+    funKingdom: "funKingdom",
+    classic: "classic",
+    kingdom: "kingdom",
+    fireAndIce: "fireAndIce",
+  };
+
+  export const gridColorsObject = gridColors
+  export const colorSelectAttributesObject = colorSelectAttributes
