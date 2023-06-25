18e55937c6d789f8bc94056d8140615977445629
mjmealey <markjmealey@gmail.com>
2023-06-24 21:24:33 -0500

    functions invoked

--- src/Modules/vsComputer/vsComputer.js
+++ src/Modules/vsComputer/vsComputer.js
@@ -1,5 +1,6 @@
 import ChessPieceAssignments from "./ChessPieceAssignments.js";
 import ChessBoardColors from "./ChessBoardColors.js";
+import ChessPieceClicks from "./ChessPieceClicks.js";
 
 const vsComputer = () => {
   const newChessPieceAssignments = ChessPieceAssignments();
@@ -9,8 +10,11 @@ const vsComputer = () => {
   newChessPieceAssignments.bishopCellAssignments()
   newChessPieceAssignments.rookCellAssignments()
   newChessPieceAssignments.knightCellAssignments()
+  newChessPieceAssignments.startOfGame()
   const newChessBoardColors = ChessBoardColors()
-  newChessBoardColors.cellColors()
+  newChessBoardColors.cellColors() 
+  const newChessPieceClick = ChessPieceClicks()
+  newChessPieceClick.clickPawnCell() 
 };
 
 export default vsComputer;
