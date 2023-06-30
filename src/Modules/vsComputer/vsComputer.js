aec1fbfca0451329820bf4b4310dec322c9426a0
mjmealey <markjmealey@gmail.com>
2023-06-30 17:47:45 -0500

    new functions invoked

--- src/Modules/vsComputer/vsComputer.js
+++ src/Modules/vsComputer/vsComputer.js
@@ -14,9 +14,11 @@ const vsComputer = () => {
   newChessBoardColors.cellColors() 
   const newPawn = Pawn()
   newPawn.PAWN_CELL_ASSIGNMENTS()
+  newPawn.WATCH_PAWN_POSITIONS()
   newPawn.DOUBLE_SPACES()
   newPawn.MOVE_ONE_SPACE_AT_START()
   newPawn.PREVENT_PAWNS_ON_FIRST_SPACES_AFTER_DOUBLE()
+  newPawn.PREVENT_PAWNS_FROM_DUPLICATING_ON_SECOND_SPACE_AFTER_FIRST_SINGLE_SPACE()
 };
 
 export default vsComputer;
