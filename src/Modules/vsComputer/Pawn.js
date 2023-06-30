a01e5431839ab9f4356e6612301e5c069ca1739e
mjmealey <markjmealey@gmail.com>
2023-06-30 17:46:58 -0500

    single spacing complete for pawns at start of game and duplication prevention is complete for the start of the game as well. starting on positioning now

--- src/Modules/vsComputer/Pawn.js
+++ src/Modules/vsComputer/Pawn.js
@@ -107,6 +107,34 @@ const Pawn = () => {
     }
     return { PAWN_CELL_ASSIGNMENTS };
   };
+
+  const WATCH_PAWN_POSITIONS = (mappedCellIds) => {
+    mappedCellIds = getCellIds.map((id) => document.getElementById(id));
+
+    gridContainer.addEventListener("click", (e) => {
+      const WATCH_PAWN_POSITION_CLICKS = e.target.id;
+
+      switch (WATCH_PAWN_POSITION_CLICKS) {
+        case getCellIds[2]:
+          if (
+            currentPawn ===
+            `${pawnAssignmentObject.pawnCellOne.pawnCellStartingPosition}`
+          ) {
+            possibleCellOnePositions[1] = `${pawnAssignmentObject.pawnCellOne.cellOnePawnOnCellTwo}`;
+            console.log(possibleCellOnePositions[1]);
+          }
+          break;
+        case getCellIds[3]:
+          if(currentPawn === `${pawnAssignmentObject.pawnCellOne.pawnCellStartingPosition}`){
+            possibleCellOnePositions[2] = `${pawnAssignmentObject.pawnCellOne.cellOnePawnOnCellThree}`
+            console.log(possibleCellOnePositions[2])
+          }
+          break;
+      }
+    });
+
+    return { WATCH_PAWN_POSITIONS };
+  };
   //logic for controlling the first move for each pawn at the start of the game
   //controls double spaces for pawns at the start of the game
   const DOUBLE_SPACES = (mappedCellIds) => {
@@ -282,7 +310,7 @@ const Pawn = () => {
             currentPawn ===
             `${pawnAssignmentObject.pawnCellFiftySeven.pawnCellStartingPosition}`
           ) {
-            console.log("hi")
+            console.log("hi");
             mappedCellIds[57].textContent = `${trackGameStateObject.emptyCell}`;
             mappedCellIds[59].textContent = `${pawnAssignmentObject.pieceName}`;
             doubleSpacePawnMove[7] = `${pawnAssignmentObject.pawnCellFiftySeven.doubleSpacePawnMoveComplete}`;
@@ -682,6 +710,7 @@ const Pawn = () => {
           ) {
             mappedCellIds[22].textContent = `${trackGameStateObject.emptyCell}`;
             mappedCellIds[21].textContent = `${pawnAssignmentObject.pieceName}`;
+            firstPawnMoves[10] = `${pawnAssignmentObject.pawnCellTwentyTwo.firstPawnMoveComplete}`;
           }
           break;
         case getCellIds[30]:
@@ -762,6 +791,26 @@ const Pawn = () => {
             mappedCellIds[53].textContent = `${pawnAssignmentObject.pieceName}`;
             firstPawnMoves[14] = `${pawnAssignmentObject.pawnCellFiftyFour.firstPawnMoveComplete}`;
           }
+          break;
+        case getCellIds[62]:
+          if (
+            mappedCellIds[62].textContent ===
+              `${pawnAssignmentObject.pieceName}` &&
+            firstPawnMoves[15] ===
+              `${pawnAssignmentObject.pawnCellSixtyTwo.firstPawnMoveNotComplete}`
+          ) {
+            currentPawn = `${pawnAssignmentObject.pawnCellSixtyTwo.pawnCellStartingPosition}`;
+          }
+          break;
+        case getCellIds[61]:
+          if (
+            currentPawn ===
+            `${pawnAssignmentObject.pawnCellSixtyTwo.pawnCellStartingPosition}`
+          ) {
+            mappedCellIds[62].textContent = `${trackGameStateObject.emptyCell}`;
+            mappedCellIds[61].textContent = `${pawnAssignmentObject.pieceName}`;
+            firstPawnMoves[15] = `${pawnAssignmentObject.pawnCellSixtyTwo.firstPawnMoveComplete}`;
+          }
       }
     });
     return { MOVE_ONE_SPACE_AT_START };
@@ -770,9 +819,9 @@ const Pawn = () => {
   const PREVENT_PAWNS_ON_FIRST_SPACES_AFTER_DOUBLE = (mappedCellIds) => {
     mappedCellIds = getCellIds.map((id) => document.getElementById(id));
     gridContainer.addEventListener("click", (e) => {
-      const PREVENT_ADDITIONAL_CLICKS = e.target.id;
+      const PREVENT_ADDITIONAL_SINGLE_SPACE_CLICKS = e.target.id;
 
-      switch (PREVENT_ADDITIONAL_CLICKS) {
+      switch (PREVENT_ADDITIONAL_SINGLE_SPACE_CLICKS) {
         //top side tracking
         case getCellIds[2]:
           if (
@@ -896,24 +945,44 @@ const Pawn = () => {
           }
           break;
         case getCellIds[37]:
-          if(mappedCellIds[36].textContent === `${pawnAssignmentObject.pieceName}` && doubleSpacePawnMove[12] === `${pawnAssignmentObject.pawnCellThirtyEight.doubleSpacePawnMoveComplete}`){
-            mappedCellIds[37].textContent = `${trackGameStateObject.emptyCell}`
+          if (
+            mappedCellIds[36].textContent ===
+              `${pawnAssignmentObject.pieceName}` &&
+            doubleSpacePawnMove[12] ===
+              `${pawnAssignmentObject.pawnCellThirtyEight.doubleSpacePawnMoveComplete}`
+          ) {
+            mappedCellIds[37].textContent = `${trackGameStateObject.emptyCell}`;
           }
           break;
         case getCellIds[45]:
-          if(mappedCellIds[44].textContent === `${pawnAssignmentObject.pieceName}` && doubleSpacePawnMove[13] === `${pawnAssignmentObject.pawnCellFortySix.doubleSpacePawnMoveComplete}`){
-            mappedCellIds[45].textContent = `${trackGameStateObject.emptyCell}`
+          if (
+            mappedCellIds[44].textContent ===
+              `${pawnAssignmentObject.pieceName}` &&
+            doubleSpacePawnMove[13] ===
+              `${pawnAssignmentObject.pawnCellFortySix.doubleSpacePawnMoveComplete}`
+          ) {
+            mappedCellIds[45].textContent = `${trackGameStateObject.emptyCell}`;
           }
-           break;
+          break;
         case getCellIds[53]:
-          if(mappedCellIds[52].textContent === `${pawnAssignmentObject.pieceName}` && doubleSpacePawnMove[14] === `${pawnAssignmentObject.pawnCellFiftyFour.doubleSpacePawnMoveComplete}`){
-            mappedCellIds[53].textContent = `${trackGameStateObject.emptyCell}`
+          if (
+            mappedCellIds[52].textContent ===
+              `${pawnAssignmentObject.pieceName}` &&
+            doubleSpacePawnMove[14] ===
+              `${pawnAssignmentObject.pawnCellFiftyFour.doubleSpacePawnMoveComplete}`
+          ) {
+            mappedCellIds[53].textContent = `${trackGameStateObject.emptyCell}`;
           }
           break;
         case getCellIds[61]:
-          if(mappedCellIds[60].textContent === `${pawnAssignmentObject.pieceName}` && doubleSpacePawnMove[15] === `${pawnAssignmentObject.pawnCellSixtyTwo.doubleSpacePawnMoveComplete}`){
-            console.log("hi")
-            mappedCellIds[61].textContent = `${trackGameStateObject.emptyCell}`
+          if (
+            mappedCellIds[60].textContent ===
+              `${pawnAssignmentObject.pieceName}` &&
+            doubleSpacePawnMove[15] ===
+              `${pawnAssignmentObject.pawnCellSixtyTwo.doubleSpacePawnMoveComplete}`
+          ) {
+            console.log("hi");
+            mappedCellIds[61].textContent = `${trackGameStateObject.emptyCell}`;
           }
           break;
       }
@@ -921,11 +990,188 @@ const Pawn = () => {
 
     return { PREVENT_PAWNS_ON_FIRST_SPACES_AFTER_DOUBLE };
   };
+
+  const PREVENT_PAWNS_FROM_DUPLICATING_ON_SECOND_SPACE_AFTER_FIRST_SINGLE_SPACE =
+    (mappedCellIds) => {
+      mappedCellIds = getCellIds.map((id) => document.getElementById(id));
+
+      gridContainer.addEventListener("click", (e) => {
+        const PREVENT_DUPLICATION_SINGLE_CLICKS = e.target.id;
+
+        switch (PREVENT_DUPLICATION_SINGLE_CLICKS) {
+          //top side pawn duplication prevention
+          case getCellIds[3]:
+            if (
+              mappedCellIds[2].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[0] ===
+                `${pawnAssignmentObject.pawnCellOne.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[3].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[11]:
+            if (
+              mappedCellIds[10].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[1] ===
+                `${pawnAssignmentObject.pawnCellNine.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[11].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[19]:
+            if (
+              mappedCellIds[18].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[2] ===
+                `${pawnAssignmentObject.pawnCellSeventeen.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[19].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[27]:
+            if (
+              mappedCellIds[26].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[3] ===
+                `${pawnAssignmentObject.pawnCellTwentyFive.firstPawnMoveComplete}`
+            ) {
+              console.log("hi");
+              mappedCellIds[27].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[35]:
+            if (
+              mappedCellIds[34].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[4] ===
+                `${pawnAssignmentObject.pawnCellThirtyThree.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[35].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[43]:
+            if (
+              mappedCellIds[42].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[5] ===
+                `${pawnAssignmentObject.pawnCellFortyOne.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[43].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[51]:
+            if (
+              mappedCellIds[50].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[6] ===
+                `${pawnAssignmentObject.pawnCellFortyNine.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[51].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[59]:
+            if (
+              mappedCellIds[58].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[7] ===
+                `${pawnAssignmentObject.pawnCellFiftySeven.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[59].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[4]:
+            if (
+              mappedCellIds[5].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[8] ===
+                `${pawnAssignmentObject.pawnCellSix.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[4].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[12]:
+            if (
+              mappedCellIds[13].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[9] ===
+                `${pawnAssignmentObject.pawnCellFourteen.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[12].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[20]:
+            if (
+              mappedCellIds[21].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[10] ===
+                `${pawnAssignmentObject.pawnCellTwentyTwo.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[20].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[28]:
+            if (
+              mappedCellIds[29].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[11] ===
+                `${pawnAssignmentObject.pawnCellThirty.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[28].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+          case getCellIds[36]:
+            if (
+              mappedCellIds[37].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[12] ===
+                `${pawnAssignmentObject.pawnCellThirtyEight.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[36].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[44]:
+            if (
+              mappedCellIds[45].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[13] ===
+                `${pawnAssignmentObject.pawnCellFortySix.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[44].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[52]:
+            if (
+              mappedCellIds[53].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[14] ===
+                `${pawnAssignmentObject.pawnCellFiftyFour.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[52].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+            break;
+          case getCellIds[60]:
+            if (
+              mappedCellIds[61].textContent ===
+                `${pawnAssignmentObject.pieceName}` &&
+              firstPawnMoves[15] ===
+                `${pawnAssignmentObject.pawnCellSixtyTwo.firstPawnMoveComplete}`
+            ) {
+              mappedCellIds[60].textContent = `${trackGameStateObject.emptyCell}`;
+            }
+        }
+      });
+      return {
+        PREVENT_PAWNS_FROM_DUPLICATING_ON_SECOND_SPACE_AFTER_FIRST_SINGLE_SPACE,
+      };
+    };
   return {
     PAWN_CELL_ASSIGNMENTS,
+    WATCH_PAWN_POSITIONS,
     MOVE_ONE_SPACE_AT_START,
-    PREVENT_PAWNS_ON_FIRST_SPACES_AFTER_DOUBLE,
     DOUBLE_SPACES,
+    PREVENT_PAWNS_ON_FIRST_SPACES_AFTER_DOUBLE,
+    PREVENT_PAWNS_FROM_DUPLICATING_ON_SECOND_SPACE_AFTER_FIRST_SINGLE_SPACE,
   };
 };
 export default Pawn;
