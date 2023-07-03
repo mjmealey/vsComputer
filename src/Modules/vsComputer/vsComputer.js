429f3a4af0b8d306e4308fd63faf0c7559fa1ec1
mjmealey <markjmealey@gmail.com>
2023-07-03 16:05:39 -0500

    pawn functions removed

--- src/Modules/vsComputer/vsComputer.js
+++ src/Modules/vsComputer/vsComputer.js
@@ -12,12 +12,7 @@ const vsComputer = () => {
   newChessPieceAssignments.startOfGame()
   const newChessBoardColors = ChessBoardColors()
   newChessBoardColors.cellColors() 
-  const newPawn = Pawn()
-  newPawn.PAWN_CELL_ASSIGNMENTS()
-  newPawn.handleComputerSideDoubleSpaceClicks()
-  newPawn.DOUBLE_SPACES_ON_BOTTOMSIDE()
-  newPawn.PREVENT_PAWNS_ON_FIRST_SPACES_AFTER_DOUBLE()
-  newPawn.PREVENT_PAWNS_FROM_DUPLICATING_ON_SECOND_SPACE_AFTER_FIRST_SINGLE_SPACE()
+  
 };
 
 export default vsComputer;
