1bdf4aff467cdddae498663e6c4343200881678b
mjmealey <markjmealey@gmail.com>
2023-07-03 21:59:29 -0500

    new functions called

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
-import vsComputer from "./Modules/vsComputer/vsComputer.js"
-import Pawn from "./Modules/vsComputer/Pawn.js"
+import vsComputer from "./Modules/vsComputer/vsComputer.js";
+import Pawn from "./Modules/vsComputer/Pawn.js";
 
+const ChessApp = () => {
+  vsComputer();
+  Pawn();
+};
 
-vsComputer()
-Pawn()
+ChessApp();
