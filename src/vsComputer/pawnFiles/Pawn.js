49c00a5f0e7e520e0ffe3dba0aa026e627c1480f
mjmealey <markjmealey@gmail.com>
2023-07-05 17:05:27 -0500

    new functions invoked

--- src/vsComputer/pawnFiles/Pawn.js
+++ src/vsComputer/pawnFiles/Pawn.js
@@ -1,49 +1,59 @@
-import PawnCellAssignments from "./modules/general-modules/PawnCellAssignments.js"
-import ComputerDoubleSpace from "./modules/computer-modules/ComputerDoubleSpace.js"
-import PlayerDoubleSpace from "./modules/player-modules/PlayerDoubleSpace.js"
-import ComputerSingleSpaceAtStart from "./modules/computer-modules/ComputerSingleSpaceAtStart.js"
-import PlayerSingleSpaceAtStart from "./modules/player-modules/PlayerSingleSpace.js"
+import PawnCellAssignments from "./modules/general-modules/PawnCellAssignments.js";
+import ComputerDoubleSpace from "./modules/computer-modules/ComputerDoubleSpace.js";
+import PlayerDoubleSpace from "./modules/player-modules/PlayerDoubleSpace.js";
+import ComputerSingleSpaceAtStart from "./modules/computer-modules/ComputerSingleSpaceAtStart.js";
+import PlayerSingleSpaceAtStart from "./modules/player-modules/PlayerSingleSpace.js";
+import PreventComputerDuplication from "./modules/computer-modules/computerPreventDuplicates.js";
 
 const Pawn = () => {
-const newPawnAssignments = PawnCellAssignments()
-newPawnAssignments.computerSideAssignments()
-newPawnAssignments.playerSideAssignments()
-const newComputerDoubleSpace = ComputerDoubleSpace()
-newComputerDoubleSpace.handleFirstComputerSideDoubleSpaceClicks() 
-newComputerDoubleSpace.handleSecondComputerSideDoubleSpaceClicks()
-newComputerDoubleSpace.handleThirdComputerSideDoubleSpaceClicks()
-newComputerDoubleSpace.handleFourthComputerSideDoubleSpaceClicks()
-newComputerDoubleSpace.handleFifthComputerSideDoubleSpaceClicks()
-newComputerDoubleSpace.handleSixthComputerSideDoubleSpaceClicks()
-newComputerDoubleSpace.handleSeventhComputerSideDoubleSpaceClicks()
-newComputerDoubleSpace.handleEighthComputerSideDoubleSpaceClicks()
-const newPlayerDoubleSpace = PlayerDoubleSpace()
-newPlayerDoubleSpace.firstPlayerSideDoubleSpaceClicks()
-newPlayerDoubleSpace.secondPlayerSideDoubleSpaceClicks()
-newPlayerDoubleSpace.thirdPlayerSideDoubleSpaceClicks()
-newPlayerDoubleSpace.fourthPlayerSideDoubleSpaceClicks()
-newPlayerDoubleSpace.fifthPlayerSideDoubleSpaceClicks()
-newPlayerDoubleSpace.sixthPlayerSideDoubleSpaceClicks()
-newPlayerDoubleSpace.seventhPlayerSideDoubleSpaceClicks()
-newPlayerDoubleSpace.eighthPlayerSideDoubleSpaceClicks()
-const newComputerSingleSpaceAtStart = ComputerSingleSpaceAtStart()
-newComputerSingleSpaceAtStart.handleFirstComputerSideDoubleSpaceAtStartClicks()
-newComputerSingleSpaceAtStart.handleSecondComputerSideSingleSpaceAtStartClicks()
-newComputerSingleSpaceAtStart.handleThirdComputerSideSingleSpaceAtStartClicks()
-newComputerSingleSpaceAtStart.handleFourthComputerSideSingleSpaceAtStartClicks()
-newComputerSingleSpaceAtStart.handleFifthComputerSideSingleSpaceAtStartClicks()
-newComputerSingleSpaceAtStart.handleSixthComputerSideSingleSpaceAtStartClicks()
-newComputerSingleSpaceAtStart.handleSeventhComputerSideSingleSpaceAtStartClicks()
-newComputerSingleSpaceAtStart.handleEighthComputerSideSingleSpaceAtStartClicks()
-const newPlayerSingleSpaceAtStart = PlayerSingleSpaceAtStart()
-newPlayerSingleSpaceAtStart.handleFirstPlayerSingleSpaceAtStartClicks()
-newPlayerSingleSpaceAtStart.handleSecondPlayerSingleSpaceAtStartClicks()
-newPlayerSingleSpaceAtStart.handleThirdPlayerSingleSpaceAtStartClicks()
-newPlayerSingleSpaceAtStart.handleFourthPlayerSingleSpaceAtStartClicks()
-newPlayerSingleSpaceAtStart.handleFifthPlayerSingleSpaceAtStartClicks()
-newPlayerSingleSpaceAtStart.handleSixthPlayerSingleSpaceAtStartClicks()
-newPlayerSingleSpaceAtStart.handleSeventhPlayerSingleSpaceAtStartClicks()
-newPlayerSingleSpaceAtStart.handleEighthPlayerSingleSpaceAtStartClicks()
-}
+  const newPawnAssignments = PawnCellAssignments();
+  newPawnAssignments.computerSideAssignments();
+  newPawnAssignments.playerSideAssignments();
+  const newComputerDoubleSpace = ComputerDoubleSpace();
+  newComputerDoubleSpace.handleFirstComputerSideDoubleSpaceClicks();
+  newComputerDoubleSpace.handleSecondComputerSideDoubleSpaceClicks();
+  newComputerDoubleSpace.handleThirdComputerSideDoubleSpaceClicks();
+  newComputerDoubleSpace.handleFourthComputerSideDoubleSpaceClicks();
+  newComputerDoubleSpace.handleFifthComputerSideDoubleSpaceClicks();
+  newComputerDoubleSpace.handleSixthComputerSideDoubleSpaceClicks();
+  newComputerDoubleSpace.handleSeventhComputerSideDoubleSpaceClicks();
+  newComputerDoubleSpace.handleEighthComputerSideDoubleSpaceClicks();
+  const newPlayerDoubleSpace = PlayerDoubleSpace();
+  newPlayerDoubleSpace.firstPlayerSideDoubleSpaceClicks();
+  newPlayerDoubleSpace.secondPlayerSideDoubleSpaceClicks();
+  newPlayerDoubleSpace.thirdPlayerSideDoubleSpaceClicks();
+  newPlayerDoubleSpace.fourthPlayerSideDoubleSpaceClicks();
+  newPlayerDoubleSpace.fifthPlayerSideDoubleSpaceClicks();
+  newPlayerDoubleSpace.sixthPlayerSideDoubleSpaceClicks();
+  newPlayerDoubleSpace.seventhPlayerSideDoubleSpaceClicks();
+  newPlayerDoubleSpace.eighthPlayerSideDoubleSpaceClicks();
+  const newComputerSingleSpaceAtStart = ComputerSingleSpaceAtStart();
+  newComputerSingleSpaceAtStart.handleFirstComputerSideDoubleSpaceAtStartClicks();
+  newComputerSingleSpaceAtStart.handleSecondComputerSideSingleSpaceAtStartClicks();
+  newComputerSingleSpaceAtStart.handleThirdComputerSideSingleSpaceAtStartClicks();
+  newComputerSingleSpaceAtStart.handleFourthComputerSideSingleSpaceAtStartClicks();
+  newComputerSingleSpaceAtStart.handleFifthComputerSideSingleSpaceAtStartClicks();
+  newComputerSingleSpaceAtStart.handleSixthComputerSideSingleSpaceAtStartClicks();
+  newComputerSingleSpaceAtStart.handleSeventhComputerSideSingleSpaceAtStartClicks();
+  newComputerSingleSpaceAtStart.handleEighthComputerSideSingleSpaceAtStartClicks();
+  const newPlayerSingleSpaceAtStart = PlayerSingleSpaceAtStart();
+  newPlayerSingleSpaceAtStart.handleFirstPlayerSingleSpaceAtStartClicks();
+  newPlayerSingleSpaceAtStart.handleSecondPlayerSingleSpaceAtStartClicks();
+  newPlayerSingleSpaceAtStart.handleThirdPlayerSingleSpaceAtStartClicks();
+  newPlayerSingleSpaceAtStart.handleFourthPlayerSingleSpaceAtStartClicks();
+  newPlayerSingleSpaceAtStart.handleFifthPlayerSingleSpaceAtStartClicks();
+  newPlayerSingleSpaceAtStart.handleSixthPlayerSingleSpaceAtStartClicks();
+  newPlayerSingleSpaceAtStart.handleSeventhPlayerSingleSpaceAtStartClicks();
+  newPlayerSingleSpaceAtStart.handleEighthPlayerSingleSpaceAtStartClicks();
+  const newPreventComputerDuplication = PreventComputerDuplication()
+  newPreventComputerDuplication.firstComputerPreventDuplicationClicks();
+  newPreventComputerDuplication.secondComputerPreventDuplicationClicks();
+  newPreventComputerDuplication.thirdComputerPreventDuplicationClicks();
+  newPreventComputerDuplication.fourthComputerPreventDuplicationClicks();
+  newPreventComputerDuplication.fifthComputerPreventDuplicationClicks();
+  newPreventComputerDuplication.sixthComputerPreventDuplicationClicks();
+  newPreventComputerDuplication.seventhComputerPreventDuplicationClicks();
+  newPreventComputerDuplication.eighthComputerPreventDuplicationClicks();
+};
 
-export default Pawn 
+export default Pawn;
