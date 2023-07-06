e939e6a9141b5759c8c4008e7a9c369f15c2bf7a
mjmealey <markjmealey@gmail.com>
2023-07-05 19:03:05 -0500

    single space at start and double space at start pawn duplication complete for computer side

--- src/vsComputer/pawnFiles/modules/computer-modules/computerPreventDuplicates.js
+++ src/vsComputer/pawnFiles/modules/computer-modules/computerPreventDuplicates.js
@@ -8,7 +8,7 @@ const PreventComputerDuplication = () => {
   const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
   const watchPieceName = `${pawnAssignmentObject.pieceName}`;
   const watchForEmptyCellAfterDoubleSpace = `${trackGameStateObject.emptyCell}`;
-
+  const watchForEmptyCellAfterSingleSpace = `${trackGameStateObject.emptyCell}`;
   const firstComputerPreventDuplicationClicks = () => {
     gridContainer.addEventListener("click", firstComputerPreventDuplication);
 
@@ -17,10 +17,11 @@ const PreventComputerDuplication = () => {
 
   const firstComputerPreventDuplication = (e) => {
     const firstDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnOne.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const firstSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnOne.watchForFirstMove.firstMoveComplete}`;
     const preventFirstDuplicationAfterDoubleSpace = e.target.id;
     switch (preventFirstDuplicationAfterDoubleSpace) {
+      //handles duplication possible when double spacing is done
       case getCellIds[2]:
-        console.log("hi");
         if (
           mappedCellIds[3].textContent === `${watchPieceName}` &&
           doubleSpacePawnMoveArray[0] === `${firstDoubleSpaceComplete}`
@@ -28,6 +29,14 @@ const PreventComputerDuplication = () => {
           mappedCellIds[2].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
         }
         break;
+      //handles duplication possible when single spacing is done at the start instead of double spacing
+      case getCellIds[3]:
+        if (
+          mappedCellIds[2].textContent === `${watchPieceName}` &&
+          singleSpaceAtStartArray[0] === `${firstSingleSpaceComplete}`
+        ) {
+          mappedCellIds[3].textContent = `${watchForEmptyCellAfterSingleSpace}`;
+        }
     }
   };
 
@@ -39,6 +48,7 @@ const PreventComputerDuplication = () => {
 
   const secondComputerPreventDuplication = (e) => {
     const secondDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnTwo.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const secondSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnTwo.watchForFirstMove.firstMoveComplete}`;
     const preventSecondDuplicationAfterDoubleSpace = e.target.id;
     switch (preventSecondDuplicationAfterDoubleSpace) {
       case getCellIds[10]:
@@ -47,6 +57,14 @@ const PreventComputerDuplication = () => {
           doubleSpacePawnMoveArray[1] === `${secondDoubleSpaceComplete}`
         ) {
           mappedCellIds[10].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
+        }
+        break;
+      case getCellIds[11]:
+        if (
+          mappedCellIds[10].textContent === `${watchPieceName}` &&
+          singleSpaceAtStartArray[1] === `${secondSingleSpaceComplete}`
+        ) {
+          mappedCellIds[11].textContent = `${watchForEmptyCellAfterSingleSpace}`;
         }
         break;
     }
@@ -60,6 +78,7 @@ const PreventComputerDuplication = () => {
 
   const thirdComputerPreventDuplication = (e) => {
     const thirdDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnThree.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const thirdSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnThree.watchForFirstMove.firstMoveComplete}`;
     const preventThirdDuplicationAfterDoubleSpace = e.target.id;
     switch (preventThirdDuplicationAfterDoubleSpace) {
       case getCellIds[18]:
@@ -68,6 +87,14 @@ const PreventComputerDuplication = () => {
           doubleSpacePawnMoveArray[2] === `${thirdDoubleSpaceComplete}`
         ) {
           mappedCellIds[18].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
+        }
+        break;
+      case getCellIds[19]:
+        if (
+          mappedCellIds[18].textContent === `${watchPieceName}` &&
+          singleSpaceAtStartArray[2] === `${thirdSingleSpaceComplete}`
+        ) {
+          mappedCellIds[19].textContent = `${watchForEmptyCellAfterSingleSpace}`;
         }
         break;
     }
@@ -81,6 +108,7 @@ const PreventComputerDuplication = () => {
 
   const fourthComputerPreventDuplication = (e) => {
     const fourthDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnFour.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const fourthSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnFour.watchForFirstMove.firstMoveComplete}`;
     const preventFourthDuplicationAfterDoubleSpace = e.target.id;
     switch (preventFourthDuplicationAfterDoubleSpace) {
       case getCellIds[26]:
@@ -91,6 +119,13 @@ const PreventComputerDuplication = () => {
           mappedCellIds[26].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
         }
         break;
+      case getCellIds[27]:
+        if (
+          mappedCellIds[26].textContent === `${watchPieceName}` &&
+          singleSpaceAtStartArray[3] === `${fourthSingleSpaceComplete}`
+        ) {
+          mappedCellIds[27].textContent = `${watchForEmptyCellAfterSingleSpace}`;
+        }
     }
   };
 
@@ -102,6 +137,7 @@ const PreventComputerDuplication = () => {
 
   const fifthComputerPreventDuplication = (e) => {
     const fifthDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnFive.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const fifthSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnFive.watchForFirstMove.firstMoveComplete}`;
     const preventFifthDuplicationAfterDoubleSpace = e.target.id;
     switch (preventFifthDuplicationAfterDoubleSpace) {
       case getCellIds[34]:
@@ -110,6 +146,14 @@ const PreventComputerDuplication = () => {
           doubleSpacePawnMoveArray[4] === `${fifthDoubleSpaceComplete}`
         ) {
           mappedCellIds[34].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
+        }
+        break;
+      case getCellIds[35]:
+        if (
+          mappedCellIds[34].textContent === `${watchPieceName}` &&
+          doubleSpacePawnMoveArray[4] === `${fifthSingleSpaceComplete}`
+        ) {
+          mappedCellIds[35].textContent = `${watchForEmptyCellAfterSingleSpace}`;
         }
     }
   };
@@ -122,6 +166,7 @@ const PreventComputerDuplication = () => {
 
   const sixthComputerPreventDuplication = (e) => {
     const sixthDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnSix.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const sixthSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnSix.watchForFirstMove.firstMoveComplete}`;
     const preventSixthDuplicationAfterDoubleSpace = e.target.id;
     switch (preventSixthDuplicationAfterDoubleSpace) {
       case getCellIds[42]:
@@ -132,6 +177,13 @@ const PreventComputerDuplication = () => {
           mappedCellIds[42].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
         }
         break;
+      case getCellIds[43]:
+        if (
+          mappedCellIds[42].textContent === `${watchPieceName}` &&
+          singleSpaceAtStartArray[5] === `${sixthSingleSpaceComplete}`
+        ) {
+          mappedCellIds[43].textContent = `${watchForEmptyCellAfterSingleSpace}`;
+        }
     }
   };
 
@@ -143,6 +195,7 @@ const PreventComputerDuplication = () => {
 
   const seventhComputerPreventDuplication = (e) => {
     const seventhDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnSeven.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const seventhSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnSeven.watchForFirstMove.firstMoveComplete}`;
     const preventSeventhDuplicationAfterDoubleSpace = e.target.id;
     switch (preventSeventhDuplicationAfterDoubleSpace) {
       case getCellIds[50]:
@@ -153,6 +206,13 @@ const PreventComputerDuplication = () => {
           mappedCellIds[50].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
         }
         break;
+      case getCellIds[51]:
+        if (
+          mappedCellIds[50].textContent === `${watchPieceName}` &&
+          singleSpaceAtStartArray[6] === `${seventhSingleSpaceComplete}`
+        ) {
+          mappedCellIds[51].textContent = `${watchForEmptyCellAfterSingleSpace}`;
+        }
     }
   };
 
@@ -164,6 +224,7 @@ const PreventComputerDuplication = () => {
 
   const eighthComputerPreventDuplication = (e) => {
     const eighthDoubleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnEight.watchForDoubleSpace.doubleSpaceMoveComplete}`;
+    const eighthSingleSpaceComplete = `${pawnAssignmentObject.computerSide.pawnEight.watchForFirstMove.firstMoveComplete}`;
     const preventEighthDuplicationAfterDoubleSpace = e.target.id;
     switch (preventEighthDuplicationAfterDoubleSpace) {
       case getCellIds[58]:
@@ -172,6 +233,14 @@ const PreventComputerDuplication = () => {
           doubleSpacePawnMoveArray[7] === `${eighthDoubleSpaceComplete}`
         ) {
           mappedCellIds[58].textContent = `${watchForEmptyCellAfterDoubleSpace}`;
+        }
+        break;
+      case getCellIds[59]:
+        if (
+          mappedCellIds[58].textContent === `${watchPieceName}` &&
+          singleSpaceAtStartArray[7] === `${eighthSingleSpaceComplete}`
+        ) {
+          mappedCellIds[59].textContent = `${watchForEmptyCellAfterSingleSpace}`;
         }
     }
   };
