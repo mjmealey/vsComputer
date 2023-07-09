3f2b59c1e4b4ff7ac0875aed3ed41fd086cf4a35
mjmealey <markjmealey@gmail.com>
2023-07-08 22:14:13 -0500

    switch statement made to watch for individual positions based on the algorithm

--- src/vsComputer/pawnFiles/modules/computer-modules/ComputerPawnSpacing.js
+++ src/vsComputer/pawnFiles/modules/computer-modules/ComputerPawnSpacing.js
@@ -1,35 +1,65 @@
+import chessBoard from "../../../objects/chessBoardArray.js";
+import { computerAssignedNamesArray } from "../../objects/pawnArrays.js";
+import { getCellIds } from "../../../modules/ChessPieceAssignments.js";
+import { pawnAssignmentObject } from "../../objects/pawnObjects.js";
+import { computerStartingRowsArray } from "../../objects/pawnArrays.js";
+import { computerStartingColumnsArray } from "../../objects/pawnArrays.js";
 
+const ComputerDoubleSpacing = () => {
+  const watchForDoubleSpacing = () => {
+    const getChessBoard = chessBoard.length;
+    const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
+    const pawnDoubleSpaceAtStart = [];
 
-//template to look at for implementing algorithms (will erase later)
-
-const ComputerPawnSpacingAtStart = () => {
-  const chessBoard = [
-    ["r", "p", "-", "-", "-", "-", "P", "R"],
-    ["k", "p", "-", "-", "-", "-", "P", "K"],
-    ["b", "p", "-", "-", "-", "-", "P", "B"],
-    ["q", "p", "-", "-", "-", "-", "P", "Q"],
-    ["k", "p", "-", "-", "-", "-", "P", "K"],
-    ["b", "p", "-", "-", "-", "-", "P", "B"],
-    ["k", "p", "-", "-", "-", "-", "P", "K"],
-    ["r", "p", "-", "-", "-", "-", "P", "R"],
-  ];
-
-  const watchForPawnDoubleSpacing = () => {
-    let pawnDoubleSpaceAtStart = [];
-
-    for (let row = 0; row < chessBoard.length; row++) {
+    let doubleSpacingUsed = false;
+    for (let row = 0; row < getChessBoard; row++) {
       for (let col = 0; col < chessBoard[row].length; col++) {
         const doubleSpacing = chessBoard[row][col];
-
-        if(doubleSpacing === "p"){
-          pawnDoubleSpaceAtStart.push({row, col})
+        /* starting row is 0 for pawns */
+        /* starting column is 1 for pawns */
+        switch (doubleSpacing) {
+          case "p1":
+            if (
+              row === computerStartingRowsArray[0] &&
+              col === computerStartingColumnsArray[0]
+            ) {
+              doubleSpacingUsed = false;
+            } else if (
+              row === computerStartingRowsArray[0] &&
+              col === computerStartingColumnsArray[0] + 1
+            ) {
+              doubleSpacingUsed = false;
+            } else if (
+              row === computerStartingRowsArray[0] &&
+              col === computerStartingColumnsArray[0] + 2
+            ) {
+              doubleSpacingUsed = true;
+              pawnDoubleSpaceAtStart.push(doubleSpacing);
+            }
+            break;
+          case "p2":
+            if (
+              row === computerStartingRowsArray[1] &&
+              computerStartingColumnsArray[1]
+            ) {
+              doubleSpacingUsed = false;
+            } else if (
+              row === computerStartingRowsArray[1] &&
+              col === computerStartingColumnsArray[1] + 1
+            ) {
+              doubleSpacingUsed = false;
+            } else if (
+              row === computerStartingRowsArray[1] &&
+              col === computerStartingColumnsArray[1] + 2
+            ) {
+              doubleSpacingUsed = true;
+            }
         }
       }
     }
-    console.log(pawnDoubleSpaceAtStart)
-    return { watchForPawnDoubleSpacing }
+    return { watchForDoubleSpacing };
   };
-  return { watchForPawnDoubleSpacing };
+  return { watchForDoubleSpacing };
 };
 
-export default ComputerPawnSpacingAtStart
+export default ComputerDoubleSpacing;
