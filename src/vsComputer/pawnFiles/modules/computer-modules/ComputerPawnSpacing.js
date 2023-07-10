d6a34103ceec34649d6e59dd5b7312a49a57f24e
mjmealey <markjmealey@gmail.com>
2023-07-09 23:02:26 -0500

    I decided to move to working on player functionality for double spacing as the computer will take much longer to finish. I am moving my code to the player file in the near future

--- src/vsComputer/pawnFiles/modules/computer-modules/ComputerPawnSpacing.js
+++ src/vsComputer/pawnFiles/modules/computer-modules/ComputerPawnSpacing.js
@@ -1,65 +1,165 @@
 import chessBoard from "../../../objects/chessBoardArray.js";
 import { getCellIds } from "../../../modules/ChessPieceAssignments.js";
 import { pawnAssignmentObject } from "../../objects/pawnObjects.js";
-import { computerStartingRowsArray } from "../../objects/pawnArrays.js";
-import { computerStartingColumnsArray } from "../../objects/pawnArrays.js";
+import { trackGameStateObject } from "../../../objects/gameStateObjects.js";
 
-const ComputerDoubleSpacing = () => {
-  const watchForDoubleSpacing = () => {
-    const getChessBoard = chessBoard.length;
+const PlayerDoubleSpacing = () => {
+  const validateDoubleSpacing = (
+    startRow,
+    startCol,
+    midRow,
+    midCol,
+    maxRow,
+    maxCol,
+    surplusRow,
+    surplusCol
+  ) => {
     const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
-    const pawnDoubleSpaceAtStart = [];
+    let doubleSpacingAtStart = [];
 
-    let doubleSpacingUsed = false;
-    for (let row = 0; row < getChessBoard; row++) {
+    for (let row = 0; row < chessBoard.length; row++) {
       for (let col = 0; col < chessBoard[row].length; col++) {
         const doubleSpacing = chessBoard[row][col];
         /* starting row is 0 for pawns */
         /* starting column is 1 for pawns */
         switch (doubleSpacing) {
           case "p1":
-            if (
-              row === computerStartingRowsArray[0] &&
-              col === computerStartingColumnsArray[0]
-            ) {
-              doubleSpacingUsed = false;
-              
+            //double spacing is done
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
             } else if (
-              row === computerStartingRowsArray[0] &&
-              col === computerStartingColumnsArray[0] + 1
+              (row === startRow && col === startCol) ||
+              (row === midRow && col === midCol) ||
+              (row >= surplusRow && col >= surplusCol)
             ) {
-              doubleSpacingUsed = false;
-            } else if (
-              row === computerStartingRowsArray[0] &&
-              col === computerStartingColumnsArray[0] + 2
-            ) {
-              doubleSpacingUsed = true;
-              pawnDoubleSpaceAtStart.push(doubleSpacing);
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p1) => remove_p1 !== "p1"
+              );
+            }
+            if (doubleSpacing === "p1") {
+              validateDoubleSpacing(0, 3, 0, 1, 0, 2, 0, 4);
             }
             break;
           case "p2":
-            if (
-              row === computerStartingRowsArray[1] &&
-              computerStartingColumnsArray[1]
-            ) {
-              doubleSpacingUsed = false;
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
             } else if (
-              row === computerStartingRowsArray[1] &&
-              col === computerStartingColumnsArray[1] + 1
+              (row === startRow && col === startCol) ||
+              (row === midRow && col === midCol) ||
+              (row >= surplusRow && col >= surplusRow)
             ) {
-              doubleSpacingUsed = false;
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p2) => remove_p2 !== "p2"
+              );
+              if (doubleSpacing === "p2") {
+                validateDoubleSpacing(1, 3, 1, 1, 1, 2, 1, 4);
+              }
+              break;
+            }
+          case "p3":
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
             } else if (
-              row === computerStartingRowsArray[1] &&
-              col === computerStartingColumnsArray[1] + 2
+              (row === midRow && col === midCol) ||
+              (row === startRow && col === startCol) ||
+              (row >= surplusRow && col >= surplusCol)
             ) {
-              doubleSpacingUsed = true;
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p3) => remove_p3 !== "p3"
+              );
+            }
+            if (doubleSpacing === "p3") {
+              validateDoubleSpacing(2, 3, 2, 1, 2, 2, 2, 4);
+            }
+            break;
+          case "p4":
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
+            } else if (
+              (row === midRow && col === midCol) ||
+              (row === startRow && col === startCol) ||
+              (row >= surplusRow && col >= surplusCol)
+            ) {
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p4) => remove_p4 !== "p4"
+              );
+            }
+            if (doubleSpacing === "p4") {
+              validateDoubleSpacing(3, 3, 3, 1, 3, 2, 3, 4);
+            }
+            break;
+          case "p5":
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
+            } else if (
+              (row === midRow && col === midCol) ||
+              (row === startRow && col === startCol) ||
+              (row >= surplusRow && col >= surplusCol)
+            ) {
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p5) => remove_p5 !== "p5"
+              );
+            }
+
+            if (doubleSpacing === "p5") {
+              validateDoubleSpacing(4, 3, 4, 1, 4, 2, 4, 4);
+            }
+            break;
+          case "p6":
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
+            } else if (
+              (row === midRow && col === midCol) ||
+              (row === startRow && col === startCol) ||
+              (row >= surplusRow && col >= surplusCol)
+            ) {
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p6) => remove_p6 !== "p6"
+              );
+            }
+            if (doubleSpacing === "p6") {
+              validateDoubleSpacing(5, 3, 5, 1, 5, 2, 5, 4);
+            }
+            break;
+          case "p7":
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
+            } else if (
+              (row === midRow && col === midCol) ||
+              (row === startRow && col === startCol) ||
+              (row >= surplusRow && col >= surplusCol)
+            ) {
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p7) => remove_p7 !== "p7"
+              );
+            }
+            if (doubleSpacing === "p7") {
+              validateDoubleSpacing(6, 3, 6, 1, 6, 2, 6, 4);
+            }
+            break;
+          case "p8":
+            if (row === maxRow && col === maxCol) {
+              doubleSpacingAtStart.push(doubleSpacing);
+            } else if (
+              (row === midRow && col === midCol) ||
+              (row === startRow && col === startCol) ||
+              (row >= surplusRow && col >= surplusCol)
+            ) {
+              doubleSpacingAtStart = doubleSpacingAtStart.filter(
+                (remove_p8) => remove_p8 !== "p8"
+              );
+            }
+            if (doubleSpacing === "p8") {
+              validateDoubleSpacing(7, 3, 7, 1, 7, 2, 7, 4);
             }
         }
       }
     }
-    return { watchForDoubleSpacing };
   };
-  return { watchForDoubleSpacing };
+
+  return {
+    watchForDoubleSpacing,
+  };
 };
 
-export default ComputerDoubleSpacing;
+export default PlayerDoubleSpacing;
