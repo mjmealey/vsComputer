5be7423e4306631791189558f9f8d337b32c221c
mjmealey <markjmealey@gmail.com>
2023-07-09 23:05:16 -0500

    function names updated

--- src/vsComputer/pawnFiles/Pawn.js
+++ src/vsComputer/pawnFiles/Pawn.js
@@ -1,11 +1,12 @@
 import PawnCellAssignments from "./modules/general-modules/PawnCellAssignments.js";
 import ComputerPawnSpacingAtStart from "./modules/computer-modules/ComputerPawnSpacing.js";
+import PlayerDoubleSpacing from "./modules/computer-modules/ComputerPawnSpacing.js";
 
 const Pawn = () => {
   const newPawnAssignments = PawnCellAssignments();
   newPawnAssignments.computerSideAssignments();
   newPawnAssignments.playerSideAssignments();
-  const newComputerPawnSpacingAtStart = ComputerPawnSpacingAtStart()
+  const newComputerPawnSpacingAtStart = PlayerDoubleSpacing()
   newComputerPawnSpacingAtStart.watchForDoubleSpacing()
 };
 
