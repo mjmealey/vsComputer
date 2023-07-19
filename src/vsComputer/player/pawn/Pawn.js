79b98ccb2c9005bd44134fc2c439064adbf3d6e4
mjmealey <markjmealey@gmail.com>
2023-07-18 21:42:25 -0500

    file paths changed and functions removed after file restructure

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -2,13 +2,8 @@ import PawnCellAssignments from "./assignPawnNames.js"
 import FirstMoves from "./modules/FirstMove.js";
 const Pawn = () => {
   const newPawnAssignments = PawnCellAssignments();
-  newPawnAssignments.computerSideAssignments();
-  newPawnAssignments.playerSideAssignments();
   const newFirstMoves = FirstMoves()
   newFirstMoves.validateFirstMoves()
-  newFirstMoves.handleP2SingleSpaceDisplay()
-  newFirstMoves.handleP2DoubleSpaceDisplay()
-
 };
 
 export default Pawn;
