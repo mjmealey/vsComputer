b69d0c0123ab4fa41d0e04f82657548fd3505a0c
mjmealey <markjmealey@gmail.com>
2023-07-18 21:47:39 -0500

    new file created to keep track of the assignment objects within arrays

---
+++ src/vsComputer/player/pawn/arrays/pawnAssignmentData.js
@@ -0,0 +1,27 @@
+import { pawnAssignments } from "../objects/pawnAssignments.js";
+
+const assignedPawnNameData = [
+  pawnAssignments.pawnOne.name,
+  pawnAssignments.pawnTwo.name,
+  pawnAssignments.pawnThree.name,
+  pawnAssignments.pawnFour.name,
+  pawnAssignments.pawnFive.name,
+  pawnAssignments.pawnSix.name,
+  pawnAssignments.pawnSeven.name,
+  pawnAssignments.pawnEight.name,
+];
+
+const assignedPawnNumberData = [
+  pawnAssignments.pawnOne.number,
+  pawnAssignments.pawnTwo.number,
+  pawnAssignments.pawnThree.number,
+  pawnAssignments.pawnFour.number,
+  pawnAssignments.pawnFive.number,
+  pawnAssignments.pawnSix.number,
+  pawnAssignments.pawnSeven.number,
+  pawnAssignments.pawnEight.number,
+];
+
+
+export const assignedPawnNames = assignedPawnNameData
+export const assignedPawnNumbers = assignedPawnNumberData
