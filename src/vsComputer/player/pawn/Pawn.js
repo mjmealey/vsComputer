a32294c1655534e0de2927824bd7a4ca9de56271
mjmealey <markjmealey@gmail.com>
2023-07-23 20:57:00 -0500

    function names and variable names changed to reflect the advancePositioning change

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -1,9 +1,13 @@
 import PawnCellAssignments from "./assignPawnNames.js"
 import FirstMoves from "./modules/FirstMove.js";
+import PawnAdvancePositioning from "./modules/AdvancePositioning.js";
+
 const Pawn = () => {
   const newPawnAssignments = PawnCellAssignments();
   const newFirstMoves = FirstMoves()
   newFirstMoves.validateFirstMoves()
+  const newAdvancePositioning = PawnAdvancePositioning()
+  newAdvancePositioning.validatePawnAdvancePositioning()
 };
 
 export default Pawn;
