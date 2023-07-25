bce50cbcb0d8ef08f98867c1a0bbf08c5a585da1
mjmealey <markjmealey@gmail.com>
2023-07-24 22:04:45 -0500

    variable added in place of 8

--- src/vsComputer/modules/ChessBoard.js
+++ src/vsComputer/modules/ChessBoard.js
@@ -2,18 +2,17 @@ import gridElements from "../objects/chessboardObjects.js";
 
 const ChessBoard = () => {
   const gridCellIds = [];
+  const gridCellCount = 8;
   const createChessBoard = () => {
- 
     const gridContainer = document.getElementById(gridElements.gridContainerId);
-    console.log(gridElements.gridContainerId);
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < gridCellCount; i++) {
       const rows = document.createElement(gridElements.div);
       rows.id = gridElements.rowId;
       rows.classList.add(gridElements.gridElementsClass);
       gridContainer.appendChild(rows);
 
-      for (let j = 0; j < 8; j++) {
+      for (let j = 0; j < gridCellCount; j++) {
         const cells = document.createElement(gridElements.div);
         const cellsId = `cell-${i}-${j}`;
         cells.id = cellsId;
@@ -23,7 +22,7 @@ const ChessBoard = () => {
         gridCellIds.push(cellsId);
       }
     }
-  
+
     return gridCellIds;
   };
   createChessBoard();
