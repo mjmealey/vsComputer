1342b59fc0a7acaf8a7b586a95f60d258a89fbf8
mjmealey <markjmealey@gmail.com>
2023-07-29 22:33:05 -0500

    file paths changed and removed

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -1,18 +1,16 @@
-import PawnCellAssignments from "./assignPawnNames.js"
-import FirstMoveDisplays from "./modules/Displays/FirstMoveDisplays.js"
-import PawnAdvancePositioning from "./modules/Positioning/AdvancePositioning.js"
+import PawnCellAssignments from "./assignPawnNames.js";
+import FirstMoveDisplays from "./modules/Displays/FirstMoveDisplays.js";
 import FirstMovePositioning from "./modules/Positioning/FirstMovePositioning.js";
+
 
 const Pawn = () => {
   const newPawnAssignments = PawnCellAssignments();
-  const newFirstMoveDisplays = FirstMoveDisplays()
-  newFirstMoveDisplays.handleStartSpaces()
-  newFirstMoveDisplays.handleSingleSpaceDisplays()
-  newFirstMoveDisplays.handleDoubleSpaceDisplay()
-  const newFirstMovePositioning = FirstMovePositioning()
-  newFirstMovePositioning.validateFirstMoves()
-  const newAdvancePositioning = PawnAdvancePositioning()
-  newAdvancePositioning.validatePawnAdvancePositioning()
+  const newFirstMoveDisplays = FirstMoveDisplays();
+  newFirstMoveDisplays.handleStartSpaces();
+  newFirstMoveDisplays.handleSingleSpaceDisplays();
+  newFirstMoveDisplays.handleDoubleSpaceDisplay();
+  const newFirstMovePositioning = FirstMovePositioning();
+  newFirstMovePositioning.validateFirstMoves();
 };
 
 export default Pawn;
