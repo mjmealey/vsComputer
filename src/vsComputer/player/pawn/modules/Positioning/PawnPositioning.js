7efc413337241b9b16514697aa9f3e4ed2bdebca
mjmealey <markjmealey@gmail.com>
2023-07-30 23:10:54 -0500

    new file made for tracking each pawn positioning function

---
+++ src/vsComputer/player/pawn/modules/Positioning/PawnPositioning.js
@@ -0,0 +1,8 @@
+import SingleSpacePositioning from "./firstMoves/SingleSpacePositioning.js";
+
+const PawnPositioning = () => {
+  const newSingleSpacePositioning = SingleSpacePositioning();
+  newSingleSpacePositioning.validateSingleSpace();
+};
+
+export default PawnPositioning;
