7d8f91249f7a7cb11b5e039924f08dc77d8a05d2
mjmealey <markjmealey@gmail.com>
2023-07-31 20:38:21 -0500

    new functions invoked

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -1,16 +1,13 @@
 import PawnCellAssignments from "./assignPawnNames.js";
 import FirstMoveDisplays from "./modules/Displays/FirstMoveDisplays.js";
-import FirstMovePositioning from "./modules/Positioning/FirstMovePositioning.js";
-
-
+import PawnPositioning from "./modules/Positioning/PawnPositioning.js";
 const Pawn = () => {
   const newPawnAssignments = PawnCellAssignments();
   const newFirstMoveDisplays = FirstMoveDisplays();
   newFirstMoveDisplays.handleStartSpaces();
   newFirstMoveDisplays.handleSingleSpaceDisplays();
   newFirstMoveDisplays.handleDoubleSpaceDisplay();
-  const newFirstMovePositioning = FirstMovePositioning();
-  newFirstMovePositioning.validateFirstMoves();
+  PawnPositioning()
 };
 
 export default Pawn;
