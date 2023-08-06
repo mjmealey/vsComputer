3d54be39d92fbec9d09a71ec1eafd6f823f3cd07
mjmealey <markjmealey@gmail.com>
2023-08-05 20:57:52 -0500

    new functions invoked

--- src/vsComputer/player/pawn/modules/Positioning/PawnPositioning.js
+++ src/vsComputer/player/pawn/modules/Positioning/PawnPositioning.js
@@ -3,7 +3,7 @@ import SingleSpacePositioning from "./firstMoves/SingleSpacePositioning.js";
 const PawnPositioning = () => {
   const newSingleSpacePositioning = SingleSpacePositioning();
   newSingleSpacePositioning.validateSingleSpace()
-
+  
 };
 
 export default PawnPositioning;
