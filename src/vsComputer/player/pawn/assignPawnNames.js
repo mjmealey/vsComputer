94ed45f03f08cbca28e0d7d2b1a4ad2545f16010
mjmealey <markjmealey@gmail.com>
2023-08-08 21:16:28 -0500

    code changed to a variable being applied in mappedCellIds instead of being passed as a parameter

--- src/vsComputer/player/pawn/assignPawnNames.js
+++ src/vsComputer/player/pawn/assignPawnNames.js
@@ -1,8 +1,8 @@
-import { getCellIds } from "../../modules/ChessPieceAssignments.js";
+import { getCellIds } from "../../modules/ChessBoard.js";
 import { pawnName } from "../pawn/objects/pawnName.js"
 
-const assignPawnNames = (mappedCellIds) => {
-  mappedCellIds = getCellIds.map((id) => document.getElementById(id));
+const assignPawnNames = () => {
+ const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
   const startingCells = [6, 14, 22, 30, 38, 46, 54, 62];
   const name = pawnName.name;
   for (let nameIndex = 0; nameIndex < getCellIds.length; nameIndex++) {
