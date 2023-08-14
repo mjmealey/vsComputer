3b5157a5c736e6658d74dbf09544cbd78ecc2da5
mjmealey <markjmealey@gmail.com>
2023-08-13 20:27:49 -0500

    variable made for array length being looped over to provide further clarification

--- src/vsComputer/player/pawn/assignPawnNames.js
+++ src/vsComputer/player/pawn/assignPawnNames.js
@@ -1,11 +1,12 @@
 import { getCellIds } from "../../modules/ChessBoard.js";
-import { pawnName } from "../pawn/objects/pawnName.js"
+import { pawnName } from "../pawn/objects/pawnName.js";
 
 const assignPawnNames = () => {
- const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
+  const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
   const startingCells = [6, 14, 22, 30, 38, 46, 54, 62];
   const name = pawnName.name;
-  for (let nameIndex = 0; nameIndex < getCellIds.length; nameIndex++) {
+  const boardCellCount = getCellIds.length //64
+  for (let nameIndex = 0; nameIndex < boardCellCount; nameIndex++) {
     if (startingCells.includes(nameIndex)) {
       mappedCellIds[nameIndex].textContent = name;
     }
@@ -13,5 +14,4 @@ const assignPawnNames = () => {
   return { assignPawnNames };
 };
 
-
-export default assignPawnNames
+export default assignPawnNames;
