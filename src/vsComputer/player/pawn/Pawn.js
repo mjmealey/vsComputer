a1dd120123eb0c62fabda102101533efcb1202d0
mjmealey <markjmealey@gmail.com>
2023-08-17 15:35:15 -0500

    file paths changed

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -2,6 +2,7 @@ import PawnCellAssignments from "./assignPawnNames.js";
 import DoubleSpaceDisplays from "./modules/Displays/DoubleSpaceDisplay.js";
 import SingleSpaceDisplays from "./modules/Displays/SingleSpaceDisplay.js";
 import PawnPositioning from "./modules/Positioning/PawnPositioning.js";
+
 const Pawn = () => {
   PawnCellAssignments();
   SingleSpaceDisplays();
