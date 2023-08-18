9efb075590c82273154f3fa911ae2cb0469e64c8
mjmealey <markjmealey@gmail.com>
2023-08-17 21:23:41 -0500

    I realized the start space file was not necessary and I am reworking the code further to be more modular by using parameters more alongside making code simpler by making the start space integrate with double and single space as before

--- src/vsComputer/player/pawn/modules/Displays/DoubleSpaceDisplay.js
+++ src/vsComputer/player/pawn/modules/Displays/DoubleSpaceDisplay.js
@@ -1,292 +1,184 @@
 import { getCellIds } from "../../../../modules/ChessBoard.js";
 import { assignedPawnNames } from "../../arrays/pawnAssignmentData.js";
 import { displayStatus } from "../../objects/displayStatus.js";
-import { trackStartSpace } from "./StartSpace.js";
+import { displayArrayRemoval } from "../../objects/startSpaceRemoval.js";
+import { assignedPawnNamesIndex } from "../../arrays/pawnAssignmentData.js";
+
 const DoubleSpaceDisplays = () => {
-  const mappedCellIds = getCellIds.map((id) => document.getElementById(id))
+  const trackDoubleSpaces = [];
+  const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
   //emptyCell is the cell the pawns start on (becomes empty after double space is complete)
-  //filledCell is the cell the pawns end on after double space
-  const doubleSpaceDisplayIndices = [
+
+  const doubleSpacePawns = [
     {
       P1: {
-        emptyCell: 6,
-        filledCell: 4,
+        assignedPawn: assignedPawnNamesIndex[0].P1,
+        startCell: getCellIds[6],
+        targetCell: getCellIds[4],
       },
+    },
+    {
       P2: {
-        emptyCell: 14,
-        filledCell: 12,
+        assignedPawn: assignedPawnNamesIndex[1].P2,
+        startCell: getCellIds[14],
+        targetCell: getCellIds[12],
       },
+    },
+    {
       P3: {
-        emptyCell: 22,
-        filledCell: 20,
+        assignedPawn: assignedPawnNamesIndex[2].P3,
+        startCell: getCellIds[22],
+        targetCell: getCellIds[20],
       },
+    },
+    {
       P4: {
-        emptyCell: 30,
-        filledCell: 28,
+        assignedPawn: assignedPawnNamesIndex[3].P4,
+        startCell: getCellIds[30],
+        targetCell: getCellIds[28],
       },
+    },
+    {
       P5: {
-        emptyCell: 38,
-        filledCell: 36,
+        assignedPawn: assignedPawnNamesIndex[4].P5,
+        startCell: getCellIds[38],
+        targetCell: getCellIds[36],
       },
+    },
+    {
       P6: {
-        emptyCell: 46,
-        filledCell: 44,
+        assignedPawn: assignedPawnNamesIndex[5].P6,
+        startCell: getCellIds[46],
+        targetCell: getCellIds[44],
       },
+    },
+    {
       P7: {
-        emptyCell: 54,
-        filledCell: 52,
+        assignedPawn: assignedPawnNamesIndex[6].P7,
+        startCell: getCellIds[54],
+        targetCell: getCellIds[52],
       },
+    },
+    {
       P8: {
-        emptyCell: 62,
-        filledCell: 60,
+        assignedPawn: assignedPawnNamesIndex[7].P8,
+        startCell: getCellIds[62],
+        targetCell: getCellIds[60],
       },
     },
   ];
 
-  const checkDoubleSpaces = (assignedPawn) => {
-    return trackPreDoubleSpace.includes(assignedPawn);
-  };
-
-  const removePreDoubleSpaceIndex = (indexToRemove, amountToRemove) => {
-    return trackPreDoubleSpace.splice(indexToRemove, amountToRemove);
-  };
-
-  const checkDoubleSpaceDisplay = (assignedPawn) => {
-    return !trackDoubleSpaces.includes(assignedPawn);
-  };
-
-  const isDoubleSpaceDisplayReady = (assignedPawn) => {
+  const preparePawns = (assignedPawn) => {
     return trackDoubleSpaces.push(assignedPawn);
   };
 
-  const isDoubleSpaceDisplayContentReady = (assignedPawn) => {
-    const isReadyToMoveFromIndex = checkDoubleSpaces(assignedPawn);
-    const isReadyToPushIntoIndex = checkDoubleSpaceDisplay(assignedPawn);
-    if (isReadyToMoveFromIndex && isReadyToPushIntoIndex) {
-      const removeFromPrevIndex = 0;
-      const amountFromPrevIndex = 1;
-      removePreDoubleSpaceIndex(removeFromPrevIndex, amountFromPrevIndex);
-      isDoubleSpaceDisplayReady(assignedPawn);
+  const removePrevPawns = (indexToRemove, amountToRemove) => {
+    return trackDoubleSpaces.splice(indexToRemove, amountToRemove);
+  };
+
+  const assignedPawnIndex = (assignedPawn) => {
+    return assignedPawnNames[assignedPawn];
+  };
+
+  const checkForPawn = (assignedPawn) => {
+    return !trackDoubleSpaces.includes(assignedPawnIndex(assignedPawn));
+  };
+
+  const indexToRemove = displayArrayRemoval.indexToRemove;
+  const amountToRemove = displayArrayRemoval.amountToRemove;
+
+  const handleStartSpaces = (assignedPawn) => {
+    const checkPawn = checkForPawn(assignedPawn);
+    if (checkPawn) {
+      removePrevPawns(indexToRemove, amountToRemove);
+      const preparedPawn = assignedPawnIndex(assignedPawn);
+      preparePawns(preparedPawn);
     }
   };
 
-  
-  const displayDoubleSpaceContent = (emptyCell, filledCell) => {
+  const preDoubleSpaceClicks = (preDoubleSpaceId) => {
+    let assignedPawn = null;
+    const P1StartCell = doubleSpacePawns[0].P1.startCell;
+    const P2StartCell = doubleSpacePawns[1].P2.startCell;
+    const P3StartCell = doubleSpacePawns[2].P3.startCell;
+    const P4StartCell = doubleSpacePawns[3].P4.startCell;
+    const P5StartCell = doubleSpacePawns[4].P5.startCell;
+    const P6StartCell = doubleSpacePawns[5].P6.startCell;
+    const P7StartCell = doubleSpacePawns[6].P7.startCell;
+    const P8StartCell = doubleSpacePawns[7].P8.startCell;
+    switch (preDoubleSpaceId) {
+      case P1StartCell:
+        assignedPawn = doubleSpacePawns[0].P1.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      case P2StartCell:
+        assignedPawn = doubleSpacePawns[1].P2.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      case P3StartCell:
+        assignedPawn = doubleSpacePawns[2].P3.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      case P4StartCell:
+        assignedPawn = doubleSpacePawns[3].P4.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      case P5StartCell:
+        assignedPawn = doubleSpacePawns[4].P5.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      case P6StartCell:
+        assignedPawn = doubleSpacePawns[5].P6.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      case P7StartCell:
+        assignedPawn = doubleSpacePawns[6].P7.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      case P8StartCell:
+        assignedPawn = doubleSpacePawns[7].P8.assignedPawn;
+        handleStartSpaces(assignedPawn);
+        console.log(trackDoubleSpaces);
+        break;
+      default:
+        return null;
+    }
+  };
+
+  const isPawnContentReady = (emptyCell, filledCell) => {
     mappedCellIds[emptyCell].textContent = displayStatus.emptyCell;
     mappedCellIds[filledCell].textContent = displayStatus.filledCell;
   };
 
-  const handleDoubleSpaceClicks = (doubleSpaceDisplayId) => {
-    switch (doubleSpaceDisplayId) {
+  const isDoubleSpacePrepared = (assignedPawn) => {
+    return trackDoubleSpaces.includes(assignedPawnIndex(assignedPawn));
+  };
+
+  const handleDoubleSpaces = (assignedPawn, emptyCell, filledCell) => {
+    const checkDoubleSpaceStatus = isDoubleSpacePrepared(assignedPawn)
+    if(checkDoubleSpaceStatus){
+      isPawnContentReady(emptyCell, filledCell)
+    }
+  };
+
+  const handleDoubleSpaceClicks = (doubleSpaceId) => {
+    switch (doubleSpaceId) {
       case getCellIds[4]:
-        const checkP1State = assignedPawnNames[0];
-        isDoubleSpaceDisplayContentReady(checkP1State);
-        break;
-      case getCellIds[12]:
-        const checkP2State = assignedPawnNames[1];
-        isDoubleSpaceDisplayContentReady(checkP2State);
-        break;
-      case getCellIds[20]:
-        const checkP3State = assignedPawnNames[2];
-        isDoubleSpaceDisplayReady(checkP3State);
-        break;
-      case getCellIds[28]:
-        const checkP4State = assignedPawnNames[3];
-        isDoubleSpaceDisplayReady(checkP4State);
-        break;
-      case getCellIds[36]:
-        const checkP5State = assignedPawnNames[4];
-        isDoubleSpaceDisplayReady(checkP5State);
-        break;
-      case getCellIds[44]:
-        const checkP6State = assignedPawnNames[5];
-        isDoubleSpaceDisplayReady(checkP6State);
-        break;
-      case getCellIds[52]:
-        const checkP7State = assignedPawnNames[6];
-        isDoubleSpaceDisplayReady(checkP7State);
-        break;
-      case getCellIds[60]:
-        const checkP8State = assignedPawnNames[7];
-        isDoubleSpaceDisplayReady(checkP8State);
-        break;
+        handleDoubleSpaces(0, 6, 4);
     }
   };
 
-  const handleDoubleSpaceDisplayClicks = (doubleSpaceDisplayId) => {
-    let emptyCell = null;
-    let filledCell = null;
-    switch (doubleSpaceDisplayId) {
-      case getCellIds[4]:
-        const isP1Display = assignedPawnNames[0];
-        const isP1DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP1Display);
-        if (isP1DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P1.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P1.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-      case getCellIds[12]:
-        const isP2Display = assignedPawnNames[1];
-        const isP2DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP2Display);
-        if (isP2DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P2.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P2.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-      case getCellIds[20]:
-        const isP3Display = assignedPawnNames[2];
-        const isP3DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP3Display);
-        if (isP3DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P3.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P3.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-      case getCellIds[28]:
-        const isP4Display = assignedPawnNames[3];
-        const isP4DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP4Display);
-        if (isP4DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P4.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P4.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-      case getCellIds[36]:
-        const isP5Display = assignedPawnNames[4];
-        const isP5DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP5Display);
-        if (isP5DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P5.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P5.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-      case getCellIds[44]:
-        const isP6Display = assignedPawnNames[5];
-        const isP6DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP6Display);
-        if (isP6DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P6.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P6.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-      case getCellIds[52]:
-        const isP7Display = assignedPawnNames[6];
-        const isP7DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP7Display);
-        if (isP7DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P7.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P7.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-      case getCellIds[60]:
-        const isP8Display = assignedPawnNames[7];
-        const isP8DoubleSpaceDisplayReady =
-          trackDoubleSpaces.includes(isP8Display);
-        if (isP8DoubleSpaceDisplayReady) {
-          emptyCell = doubleSpaceDisplayIndices[0].P8.emptyCell;
-          filledCell = doubleSpaceDisplayIndices[0].P8.filledCell;
-          displayDoubleSpaceContent(emptyCell, filledCell);
-        }
-        break;
-    }
-  };
-
-  const handleDuplicationAfterDoubleSpace = (assignedPawn) => {
-    return trackDoubleSpaces.includes(assignedPawn);
-  };
-
-  const handleEmptyCellsAfterDoubleSpace = (assignedPawn, emptyCell) => {
-    const isDuplicationPossible =
-      handleDuplicationAfterDoubleSpace(assignedPawn);
-    if (isDuplicationPossible) {
-      const emptyCellAfterDoubleSpace = displayStatus.emptyCell;
-      mappedCellIds[emptyCell].textContent = emptyCellAfterDoubleSpace;
-    }
-  };
-
-  const handleDuplicationAfterDoubleSpaceClicks = (doubleSpaceDisplayId) => {
-    const duplicationStatus = [
-      { duplicationCell: 5, duplicationPawn: assignedPawnNames[0] },
-      { duplicationCell: 13, duplicationPawn: assignedPawnNames[1] },
-      { duplicationCell: 21, duplicationPawn: assignedPawnNames[2] },
-      { duplicationCell: 29, duplicationPawn: assignedPawnNames[3] },
-      { duplicationCell: 37, duplicationPawn: assignedPawnNames[4] },
-      { duplicationCell: 45, duplicationPawn: assignedPawnNames[5] },
-      { duplicationCell: 53, duplicationPawn: assignedPawnNames[6] },
-      { duplicationCell: 61, duplicationPawn: assignedPawnNames[7] },
-    ];
-    let duplicationCell = null;
-    let duplicationPawn = null;
-    switch (doubleSpaceDisplayId) {
-      case getCellIds[5]:
-        duplicationPawn = duplicationStatus[0].duplicationPawn;
-        duplicationCell = duplicationStatus[0].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-        break;
-      case getCellIds[13]:
-        duplicationPawn = duplicationStatus[1].duplicationPawn;
-        duplicationCell = duplicationStatus[1].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-        break;
-      case getCellIds[21]:
-        duplicationPawn = duplicationStatus[2].duplicationPawn;
-        duplicationCell = duplicationStatus[2].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-        break;
-      case getCellIds[29]:
-        duplicationPawn = duplicationStatus[3].duplicationPawn;
-        duplicationCell = duplicationStatus[3].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-        break;
-      case getCellIds[37]:
-        duplicationPawn = duplicationStatus[4].duplicationPawn;
-        duplicationCell = duplicationStatus[4].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-        break;
-      case getCellIds[45]:
-        duplicationPawn = duplicationStatus[5].duplicationPawn;
-        duplicationCell = duplicationStatus[5].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-        break;
-      case getCellIds[53]:
-        duplicationPawn = duplicationStatus[6].duplicationPawn;
-        duplicationCell = duplicationStatus[6].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-        break;
-      case getCellIds[61]:
-        duplicationPawn = duplicationStatus[7].duplicationPawn;
-        duplicationCell = duplicationStatus[7].duplicationCell;
-        handleEmptyCellsAfterDoubleSpace(duplicationPawn, duplicationCell);
-    }
-  };
-
-  const handleClicks = () => {
-    document.addEventListener("click", (e) => {
-      const isDoubleSpaceClicksReady = e.target.id;
-      removeFirstMoveClicks(isDoubleSpaceClicksReady);
-      prepareFirstMoveClicks(isDoubleSpaceClicksReady);
-      handleDoubleSpaceClicks(isDoubleSpaceClicksReady);
-      handleDoubleSpaceDisplayClicks(isDoubleSpaceClicksReady);
-      handleDuplicationAfterDoubleSpaceClicks(isDoubleSpaceClicksReady);
-    });
-  };
-
-  handleClicks();
-
-  return {
-    checkDoubleSpaces,
-    checkDoubleSpaceDisplay,
-    isDoubleSpaceDisplayContentReady,
-  };
+  gridContainer.addEventListener("click", (e) => {
+    const doubleSpaceId = e.target.id;
+    preDoubleSpaceClicks(doubleSpaceId);
+    handleDoubleSpaceClicks(doubleSpaceId);
+  });
 };
-
 export default DoubleSpaceDisplays;
