207a02d9a73c5aca9b9c68d6f202ca17e514faab
mjmealey <markjmealey@gmail.com>
2023-08-18 20:37:23 -0500

    code added to double space display and the code is back to allowing for double space occured with simpler and more precise code utilizing parameters further

--- src/vsComputer/player/pawn/modules/Displays/DoubleSpaceDisplay.js
+++ src/vsComputer/player/pawn/modules/Displays/DoubleSpaceDisplay.js
@@ -13,57 +13,57 @@ const DoubleSpaceDisplays = () => {
     {
       P1: {
         assignedPawn: assignedPawnNamesIndex[0].P1,
-        startCell: getCellIds[6],
-        targetCell: getCellIds[4],
+        startCell: 6,
+        targetCell: 4,
       },
     },
     {
       P2: {
         assignedPawn: assignedPawnNamesIndex[1].P2,
-        startCell: getCellIds[14],
-        targetCell: getCellIds[12],
+        startCell: 14,
+        targetCell: 12,
       },
     },
     {
       P3: {
         assignedPawn: assignedPawnNamesIndex[2].P3,
-        startCell: getCellIds[22],
-        targetCell: getCellIds[20],
+        startCell: 22,
+        targetCell: 20,
       },
     },
     {
       P4: {
         assignedPawn: assignedPawnNamesIndex[3].P4,
-        startCell: getCellIds[30],
-        targetCell: getCellIds[28],
+        startCell: 30,
+        targetCell: 28,
       },
     },
     {
       P5: {
         assignedPawn: assignedPawnNamesIndex[4].P5,
-        startCell: getCellIds[38],
-        targetCell: getCellIds[36],
+        startCell: 38,
+        targetCell: 36,
       },
     },
     {
       P6: {
         assignedPawn: assignedPawnNamesIndex[5].P6,
-        startCell: getCellIds[46],
-        targetCell: getCellIds[44],
+        startCell: 46,
+        targetCell: 44,
       },
     },
     {
       P7: {
         assignedPawn: assignedPawnNamesIndex[6].P7,
-        startCell: getCellIds[54],
-        targetCell: getCellIds[52],
+        startCell: 54,
+        targetCell: 52,
       },
     },
     {
       P8: {
         assignedPawn: assignedPawnNamesIndex[7].P8,
-        startCell: getCellIds[62],
-        targetCell: getCellIds[60],
+        startCell: 62,
+        targetCell: 60,
       },
     },
   ];
@@ -98,14 +98,14 @@ const DoubleSpaceDisplays = () => {
 
   const preDoubleSpaceClicks = (preDoubleSpaceId) => {
     let assignedPawn = null;
-    const P1StartCell = doubleSpacePawns[0].P1.startCell;
-    const P2StartCell = doubleSpacePawns[1].P2.startCell;
-    const P3StartCell = doubleSpacePawns[2].P3.startCell;
-    const P4StartCell = doubleSpacePawns[3].P4.startCell;
-    const P5StartCell = doubleSpacePawns[4].P5.startCell;
-    const P6StartCell = doubleSpacePawns[5].P6.startCell;
-    const P7StartCell = doubleSpacePawns[6].P7.startCell;
-    const P8StartCell = doubleSpacePawns[7].P8.startCell;
+    const P1StartCell = getCellIds[6];
+    const P2StartCell = getCellIds[14];
+    const P3StartCell = getCellIds[22];
+    const P4StartCell = getCellIds[30];
+    const P5StartCell = getCellIds[38];
+    const P6StartCell = getCellIds[46];
+    const P7StartCell = getCellIds[54];
+    const P8StartCell = getCellIds[62];
     switch (preDoubleSpaceId) {
       case P1StartCell:
         assignedPawn = doubleSpacePawns[0].P1.assignedPawn;
@@ -162,16 +162,75 @@ const DoubleSpaceDisplays = () => {
   };
 
   const handleDoubleSpaces = (assignedPawn, emptyCell, filledCell) => {
-    const checkDoubleSpaceStatus = isDoubleSpacePrepared(assignedPawn)
-    if(checkDoubleSpaceStatus){
-      isPawnContentReady(emptyCell, filledCell)
+    const checkDoubleSpaceStatus = isDoubleSpacePrepared(assignedPawn);
+    if (checkDoubleSpaceStatus) {
+      isPawnContentReady(emptyCell, filledCell);
     }
   };
 
   const handleDoubleSpaceClicks = (doubleSpaceId) => {
+    let assignedIndex = null;
+    let startCell = null;
+    let targetCell = null;
+    const targetP1Cell = getCellIds[4];
+    const targetP2Cell = getCellIds[12];
+    const targetP3Cell = getCellIds[20];
+    const targetP4Cell = getCellIds[28];
+    const targetP5Cell = getCellIds[36];
+    const targetP6Cell = getCellIds[44];
+    const targetP7Cell = getCellIds[52];
+    const targetP8Cell = getCellIds[60];
     switch (doubleSpaceId) {
-      case getCellIds[4]:
-        handleDoubleSpaces(0, 6, 4);
+      case targetP1Cell:
+        assignedIndex = assignedPawnNamesIndex[0].P1;
+        startCell = doubleSpacePawns[0].P1.startCell;
+        targetCell = doubleSpacePawns[0].P1.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      case targetP2Cell:
+        assignedIndex = assignedPawnNamesIndex[1].P2;
+        startCell = doubleSpacePawns[1].P2.startCell;
+        targetCell = doubleSpacePawns[1].P2.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      case targetP3Cell:
+        assignedIndex = assignedPawnNamesIndex[2].P3;
+        startCell = doubleSpacePawns[2].P3.startCell;
+        targetCell = doubleSpacePawns[2].P3.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      case targetP4Cell:
+        assignedIndex = assignedPawnNamesIndex[3].P4;
+        startCell = doubleSpacePawns[3].P4.startCell;
+        targetCell = doubleSpacePawns[3].P4.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      case targetP5Cell:
+        assignedIndex = assignedPawnNamesIndex[4].P5;
+        startCell = doubleSpacePawns[4].P5.startCell;
+        targetCell = doubleSpacePawns[4].P5.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      case targetP6Cell:
+        assignedIndex = assignedPawnNamesIndex[5].P6;
+        startCell = doubleSpacePawns[5].P6.startCell;
+        targetCell = doubleSpacePawns[5].P6.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      case targetP7Cell:
+        assignedIndex = assignedPawnNamesIndex[6].P7;
+        startCell = doubleSpacePawns[6].P7.startCell;
+        targetCell = doubleSpacePawns[6].P7.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      case targetP8Cell:
+        assignedIndex = assignedPawnNamesIndex[7].P8;
+        startCell = doubleSpacePawns[7].P8.startCell;
+        targetCell = doubleSpacePawns[7].P8.targetCell;
+        handleDoubleSpaces(assignedIndex, startCell, targetCell);
+        break;
+      default:
+        return null;
     }
   };
 
