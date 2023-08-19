d4b6e39e5750dcdfd308340a5e077ea6a1697a71
mjmealey <markjmealey@gmail.com>
2023-08-18 20:40:52 -0500

    new idea reached where I am now implementing empty cells for single space in a separate switch statement and the rest of the code has been refactored to utilize start spaces and the target spaces in the same module

--- src/vsComputer/player/pawn/modules/Displays/SingleSpaceDisplay.js
+++ src/vsComputer/player/pawn/modules/Displays/SingleSpaceDisplay.js
@@ -1,134 +1,188 @@
 import { getCellIds } from "../../../../modules/ChessBoard.js";
 import { assignedPawnNames } from "../../arrays/pawnAssignmentData.js";
-import { removePawns } from "../../objects/removePawns.js";
+import { displayArrayRemoval } from "../../objects/startSpaceRemoval.js";
 import { displayStatus } from "../../objects/displayStatus.js";
-
+import { assignedPawnNamesIndex } from "../../arrays/pawnAssignmentData.js";
 
 const SingleSpaceDisplays = () => {
-  const indexToRemove = removePawns.indexToRemove
-  const amountToRemove = removePawns.amountToRemove
+  const trackSingleSpace = [];  
   const mappedCellIds = getCellIds.map((id) => document.getElementById(id));
-  
-  
-  const checkPawnsForSingleSpaceDisplay = (assignedPawn) => {
-    return trackStartSpace.includes(assignedPawn);
-  };
-
-  const isPawnSingleSpaceDisplayReady = (assignedPawn) => {
-    return trackStartSpace.push(assignedPawn);
-  };
-
-  const completePawnSingleSpaceDisplay = (
-    assignedPawn,
-    emptyCell,
-    displayPawn
-  ) => {
-
-    const emptyCellAfterSingleSpace = displayStatus.emptyCell;
-    const filledCellAfterSingleSpace = displayStatus.filledCell;
-    if (checkPawnsForSingleSpaceDisplay(assignedPawn)) {
-      isPawnSingleSpaceDisplayReady(assignedPawn);
-      mappedCellIds[emptyCell].textContent = emptyCellAfterSingleSpace;
-      mappedCellIds[displayPawn].textContent = filledCellAfterSingleSpace;
-      trackStartSingleSpace.splice(indexToRemove, amountToRemove);
-    }
-  };
-
-  const isPawnSingleSpaceDisplayClicksReady = (singleSpaceDisplayId) => {
-    const singleSpaceStatus = [{
+  let assignedPawn = null
+  const singleSpacePawns = [
+    {
       P1: {
-        emptyCell: 6,
-        filledCell: 5
+        assignedPawn: assignedPawnNamesIndex[0].P1,
+        startCell: 6,
+        targetCell: 5,
       },
+    },
+    {
       P2: {
-        emptyCell: 14,
-        filledCell: 13
+        assignedPawn: assignedPawnNamesIndex[1].P2,
+        startCell: 14,
+        targetCell: 13,
       },
+    },
+    {
       P3: {
-        emptyCell: 22,
-        filledCell: 21
+        assignedPawn: assignedPawnNamesIndex[2].P3,
+        startCell: 22,
+        targetCell: 21,
       },
+    },
+    {
       P4: {
-        emptyCell: 30,
-        filledCell: 29
+        assignedPawn: assignedPawnNamesIndex[3].P4,
+        startCell: 30,
+        targetCell: 29,
       },
+    },
+    {
       P5: {
-        emptyCell: 38,
-        filledCell: 37
+        assignedPawn: assignedPawnNamesIndex[4].P5,
+        startCell: 38,
+        targetCell: 37,
       },
+    },
+    {
       P6: {
-        emptyCell: 46,
-        filledCell: 45
+        assignedPawn: assignedPawnNamesIndex[5].P6,
+        startCell: 46,
+        targetCell: 45,
       },
+    },
+    {
       P7: {
-        emptyCell: 54,
-        filledCell: 53
+        assignedPawn: assignedPawnNamesIndex[6].P7,
+        startCell: 54,
+        targetCell: 53,
       },
+    },
+    {
       P8: {
-        emptyCell: 62,
-        filledCell: 61
-      }
-    }]
+        assignedPawn: assignedPawnNamesIndex[7].P8,
+        startCell: 62,
+        targetCell: 61,
+      },
+    },
+  ];
+  
 
-    switch (singleSpaceDisplayId) {
-      case getCellIds[5]:
-        const assignedP1 = assignedPawnNames[0];
-        const emptyP1Cell = singleSpaceStatus[0].P1.emptyCell
-        const filledP1Cell = singleSpaceStatus[0].P1.filledCell
-        completePawnSingleSpaceDisplay(assignedP1, emptyP1Cell, filledP1Cell);
-        break;
-      case getCellIds[13]:
-        const assignedP2 = assignedPawnNames[1];
-        const emptyP2Cell = singleSpaceStatus[0].P2.emptyCell
-        const filledP2Cell = singleSpaceStatus[0].P2.filledCell
-        completePawnSingleSpaceDisplay(assignedP2, emptyP2Cell, filledP2Cell);
-        break;
-      case getCellIds[21]:
-        const assignedP3 = assignedPawnNames[2];
-        const emptyP3Cell = singleSpaceStatus[0].P3.emptyCell
-        const filledP3Cell = singleSpaceStatus[0].P3.filledCell
-        completePawnSingleSpaceDisplay(assignedP3, emptyP3Cell, filledP3Cell);
-        break;
-      case getCellIds[29]:
-        const assignedP4 = assignedPawnNames[3];
-        const emptyP4Cell = singleSpaceStatus[0].P4.emptyCell
-        const filledP4Cell = singleSpaceStatus[0].P4.filledCell
-        completePawnSingleSpaceDisplay(assignedP4, emptyP4Cell, filledP4Cell);
-        break;
-      case getCellIds[37]:
-        const assignedP5 = assignedPawnNames[4];
-        const emptyP5Cell = singleSpaceStatus[0].P5.emptyCell
-        const filledP5Cell = singleSpaceStatus[0].P5.filledCell
-        completePawnSingleSpaceDisplay(assignedP5, emptyP5Cell, filledP5Cell);
-        break;
-      case getCellIds[45]:
-        const assignedP6 = assignedPawnNames[5];
-        const emptyP6Cell = singleSpaceStatus[0].P6.emptyCell
-        const filledP6Cell = singleSpaceStatus[0].P6.filledCell
-        completePawnSingleSpaceDisplay(assignedP6, emptyP6Cell, filledP6Cell);
-        break;
-      case getCellIds[53]:
-        const assignedP7 = assignedPawnNames[6];
-        const emptyP7Cell = singleSpaceStatus[0].P7.emptyCell
-        const filledP7Cell = singleSpaceStatus[0].P7.filledCell
-        completePawnSingleSpaceDisplay(assignedP7, emptyP7Cell, filledP7Cell);
-        break;
-      case getCellIds[61]:
-        const assignedP8 = assignedPawnNames[7];
-        const emptyP8Cell = singleSpaceStatus[0].P8.emptyCell
-        const filledP8Cell = singleSpaceStatus[0].P8.filledCell
-        completePawnSingleSpaceDisplay(assignedP8, emptyP8Cell, filledP8Cell);
+  const checkForEmptySelection = (assignedPawn) => {
+    return !trackSingleSpace.includes(assignedPawnNames[assignedPawn]);
+  };
+
+  const removePreviousPawn = (indexToRemove, amountToRemove) => {
+    return trackSingleSpace.splice(indexToRemove, amountToRemove);
+  };
+
+  const addNextPawn = (assignedPawn) => {
+    return trackSingleSpace.push(assignedPawnNames[assignedPawn]);
+  };
+
+  const isPawnReady = (assignedPawn) => {
+    const isEmptyPawnSelection = checkForEmptySelection(assignedPawn);
+    if (isEmptyPawnSelection) {
+      const indexToRemove = 0;
+      const amountToRemove = 1;
+      removePreviousPawn(indexToRemove, amountToRemove);
+      addNextPawn(assignedPawn);
+      console.log(trackSingleSpace);
     }
   };
+
+  const handlePreSingleSpaceClicks = (preSingleSpaceId) => {
+    let assignedPawn = null;
+    const startP1Cell = getCellIds[6];
+    const startP2Cell = getCellIds[14];
+    const startP3Cell = getCellIds[22];
+    const startP4Cell = getCellIds[30];
+    const startP5Cell = getCellIds[38];
+    const startP6Cell = getCellIds[46];
+    const startP7Cell = getCellIds[54];
+    const startP8Cell = getCellIds[62];
+    switch (preSingleSpaceId) {
+      case startP1Cell:
+        assignedPawn = assignedPawnNamesIndex[0].P1;
+        isPawnReady(assignedPawn);
+        break;
+      case startP2Cell:
+        assignedPawn = assignedPawnNamesIndex[1].P2;
+        isPawnReady(assignedPawn);
+        break;
+      case startP3Cell:
+        assignedPawn = assignedPawnNamesIndex[2].P3;
+        isPawnReady(assignedPawn);
+        break;
+      case startP4Cell:
+        assignedPawn = assignedPawnNamesIndex[3].P4;
+        isPawnReady(assignedPawn);
+        break;
+      case startP5Cell:
+        assignedPawn = assignedPawnNamesIndex[4].P5;
+        isPawnReady(assignedPawn);
+        break;
+      case startP6Cell:
+        assignedPawn = assignedPawnNamesIndex[5].P6;
+        isPawnReady(assignedPawn);
+        break;
+      case startP7Cell:
+        assignedPawn = assignedPawnNamesIndex[6].P7;
+        isPawnReady(assignedPawn);
+        break;
+      case startP8Cell:
+        assignedPawn = assignedPawnNamesIndex[7].P8;
+        isPawnReady(assignedPawn);
+        break;
+      default:
+        return null;
+    }
+  };
+
+  const singleSpaceP1Cell = getCellIds[5]
+  const singleSpaceP2Cell = getCellIds[13]
+  const singleSpaceP3Cell = getCellIds[21]
+  const singleSpaceP4Cell = getCellIds[29]
+  const singleSpaceP5Cell = getCellIds[37]
+  const singleSpaceP6Cell = getCellIds[45]
+  const singleSpaceP7Cell = getCellIds[53]
+  const singleSpaceP8Cell = getCellIds[61]
+
+  const isPawnReadyForSingleSpace = (assignedPawn) => {
+    return trackSingleSpace.includes(assignedPawnNames[assignedPawn]);
+  };
+
+  const emptyCellContent = (emptyCell) => {
+    const emptyCellAfterSingleSpace = displayStatus.emptyCell;
+    mappedCellIds[emptyCell].textContent = emptyCellAfterSingleSpace;
+  };
+
+  const isEmptyCellReady = (assignedPawn, emptyCell) => {
+    const isEmptyCell = isPawnReadyForSingleSpace(assignedPawn)
+    if(isEmptyCell){
+      emptyCellContent(emptyCell)
+    }
+  }
+
+  const emptyCellsAfterSingleSpace = (singleSpaceId) => {
+    let emptyCell = null
+    switch(singleSpaceId){
+      case singleSpaceP1Cell:
+          assignedPawn = assignedPawnNamesIndex[0].P1
+          emptyCell = singleSpacePawns[0].P1.startCell
+          isEmptyCellReady(assignedPawn, emptyCell)
+          break;
+        case singleSpaceP2Cell:
+    }
+  }
+
+  
 
   gridContainer.addEventListener("click", (e) => {
-    const handleSingleSpaceClicks = e.target.id;
-    isPawnSingleSpaceDisplayClicksReady(handleSingleSpaceClicks);
+    const singleSpaceId = e.target.id;
+    handlePreSingleSpaceClicks(singleSpaceId);
+    emptyCellsAfterSingleSpace(singleSpaceId)
   });
-
-  return {
-    SingleSpaceDisplays,
-  };
 };
 
 export default SingleSpaceDisplays;
