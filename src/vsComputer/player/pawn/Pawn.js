13747c0be9b7c3e7e1271ae36562791e38cfbcc9
mjmealey <markjmealey@gmail.com>
2023-08-19 21:13:32 -0500

    new function invoked, single and double space display functions removed

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -1,12 +1,10 @@
 import PawnCellAssignments from "./assignPawnNames.js";
-import DoubleSpaceDisplays from "./modules/Displays/DoubleSpaceDisplay.js";
-import SingleSpaceDisplays from "./modules/Displays/SingleSpaceDisplay.js";
+import FirstMoveDisplays from "./modules/Displays/FirstMoveDisplays.js";
 import PawnPositioning from "./modules/Positioning/PawnPositioning.js";
 
 const Pawn = () => {
   PawnCellAssignments();
-  SingleSpaceDisplays();
-  DoubleSpaceDisplays();
+  FirstMoveDisplays();
   PawnPositioning();
 };
 
