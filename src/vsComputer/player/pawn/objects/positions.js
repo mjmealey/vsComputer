851edf10ba8c25282c215d55388a176c9e521f85
mjmealey <markjmealey@gmail.com>
2023-08-23 20:58:43 -0500

    file made to track pawn positions

---
+++ src/vsComputer/player/pawn/objects/positions.js
@@ -0,0 +1,18 @@
+const pawnPositions = {
+  P1: {
+    startSpace: {
+      row: 0,
+      col: 6,
+    },
+    singleSpace: {
+      row: 0,
+      col: 5,
+    },
+    doubleSpace: {
+      row: 0,
+      col: 4,
+    },
+  },
+};
+
+export default pawnPositions;
