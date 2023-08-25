a8ff17d225ac4f67f1e51fc6ab86ff7f55d0c7fa
mjmealey <markjmealey@gmail.com>
2023-08-24 21:12:26 -0500

    invoke new display and position functions to route to app.js file

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -1,12 +1,12 @@
 import PawnCellAssignments from "./assignPawnNames.js";
-import FirstMoveDisplays from "./modules/Displays/FirstMoveDisplays.js";
+import handleP1 from "./modules/Displays/P1/handleP1Displays.js";
+import handleP2Displays from "./modules/Displays/P2/handleP2Displays.js";
 import PawnPositioning from "./modules/Positioning/PawnPositioning.js";
-
-import handleP1 from "./modules/P1/handleP1.js";
-
 const Pawn = () => {
   PawnCellAssignments();
   handleP1()
+  handleP2Displays()
+  PawnPositioning()
 };
 
 export default Pawn;
