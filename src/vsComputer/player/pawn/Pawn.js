7380e5468df6d6ab2528703677b0a7160737f61b
mjmealey <markjmealey@gmail.com>
2023-08-26 21:22:28 -0500

    change function and import names due to prior file and function name changes

--- src/vsComputer/player/pawn/Pawn.js
+++ src/vsComputer/player/pawn/Pawn.js
@@ -3,6 +3,7 @@ import handleP1 from "./modules/Displays/P1/handleP1Displays.js";
 import handleP2Displays from "./modules/Displays/P2/handleP2Displays.js";
 import handleP3Displays from "./modules/Displays/P3/handleP3Displays.js";
 import handleP4Displays from "./modules/Displays/P4/handleP4Displays.js";
+import handleP5Displays from "./modules/Displays/P5/handleP5Displays.js";
 import PawnPositioning from "./modules/Positioning/PawnPositioning.js";
 const Pawn = () => {
   PawnCellAssignments();
@@ -10,6 +11,7 @@ const Pawn = () => {
   handleP2Displays()
   handleP3Displays()
   handleP4Displays()
+  handleP5Displays()
   PawnPositioning()
 };
 
