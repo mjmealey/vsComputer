a8d39bccf96027bc63d6b9b75f00e0ef4469813b
mjmealey <markjmealey@gmail.com>
2023-08-29 21:07:53 -0500

    create an interface to give each property in removePawns a type to simplify the main code

---
+++ src/vsComputer/player/pawn/objects/removePawns.ts
@@ -0,0 +1,11 @@
+interface RemovePawns {
+  indexToRemove: number;
+  amountToRemove: number;
+}
+
+const removePawns: RemovePawns = {
+  indexToRemove: 0,
+  amountToRemove: 1,
+};
+
+export default removePawns;
