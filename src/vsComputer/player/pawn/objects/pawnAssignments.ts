95abf053fef44da308cb28874deb4130b47aaf1a
mjmealey <markjmealey@gmail.com>
2023-08-29 21:06:43 -0500

    use an interface in typescript to redo pawnAssignments and remove assignedPawnNames to simplify and allow the pawnAssignments object to be included

---
+++ src/vsComputer/player/pawn/objects/pawnAssignments.ts
@@ -0,0 +1,55 @@
+interface PawnAssignmentData {
+  pawnOne: {
+    name: string;
+  };
+  pawnTwo: {
+    name: string;
+  };
+  pawnThree: {
+    name: string;
+  };
+  pawnFour: {
+    name: string;
+  };
+  pawnFive: {
+    name: string;
+  };
+  pawnSix: {
+    name: string;
+  };
+  pawnSeven: {
+    name: string;
+  };
+  pawnEight: {
+    name: string;
+  };
+}
+
+const pawnAssignmentData: PawnAssignmentData = {
+  pawnOne: {
+    name: "P1",
+  },
+  pawnTwo: {
+    name: "P2",
+  },
+  pawnThree: {
+    name: "P3",
+  },
+  pawnFour: {
+    name: "P4",
+  },
+  pawnFive: {
+    name: "P5",
+  },
+  pawnSix: {
+    name: "P6",
+  },
+  pawnSeven: {
+    name: "P7",
+  },
+  pawnEight: {
+    name: "P8",
+  },
+};
+
+export const pawnAssignments = pawnAssignmentData;
