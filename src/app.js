0d8455b186e5ba34221a776c96991dc08c2ce7bd
mjmealey <markjmealey@gmail.com>
2023-09-03 21:11:05 -0500

    change order of functions being called

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import vsComputer from "./vsComputer/compiledTypescript/vsComputer.js"
-import Pawn from "./vsComputer/player/pawn/Pawn.js"
+import createChessBoard from "./dist/chessBoard/modules/ChessBoard.js"
+import ChessBoardColors from "./dist/chessBoard/modules/ChessBoardColors.js"
+import Pawn from "./dist/chessPieces/pawn/player/Pawn.js"
 
 const ChessApp = () => {
-  vsComputer();
-  Pawn();
+  createChessBoard()
+  ChessBoardColors()
+  Pawn()
 };
 
 ChessApp();
