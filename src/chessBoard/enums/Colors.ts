b3434d696c8a7097b2de90e528cec80bccbcb843
mjmealey <markjmealey@gmail.com>
2023-09-04 20:03:47 -0500

    rename file to Colors.ts

---
+++ src/chessBoard/enums/Colors.ts
@@ -0,0 +1,41 @@
+ export enum Colors {
+  defaultBisque = "bisque",
+  defaultBrown = "#4b2424",
+  //woodland grid
+  woodGreen = "green",
+  woodBrown = "#8B4513",
+  //checker grid
+  checkerRed = "red",
+  checkerBlack = "black",
+  //blue sky grid
+  skyBlue = "blue",
+  skyWhite = "white",
+  //fun kingdom grid
+  funKingdomPurple = "purple",
+  funKingdomPink = "pink",
+  //classic grid
+  classicBlack = "black",
+  classicWhite = "white",
+  //kingdom grid
+  vikingYellow = "yellow",
+  vikingBlue = "blue",
+  //fire and ice grid
+  fireRed = "crimson",
+  iceBlue = "lightblue",
+}
+
+export enum ColorSelect {
+    //id
+  selectId = "colorSelect",
+  //values
+  selectColor = "selectColor",
+  default = "default",
+  woodland = "woodland",
+  checker = "checker",
+  sky = "sky",
+  funKingdom = "funKingdom",
+  classic = "classic",
+  viking = "viking",
+  fireAndIce = "fireAndIce",
+}
+
