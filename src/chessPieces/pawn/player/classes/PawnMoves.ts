dd58eee4663a159eed07b761547326abb71c623a
mjmealey <markjmealey@gmail.com>
2023-09-08 17:14:40 -0500

    logic for pawn's first moves figured out, refactoring using classes to allow for multiple pieces to work at once due to each pawn having the same moveset

---
+++ src/chessPieces/pawn/player/classes/PawnMoves.ts
@@ -0,0 +1,7 @@
+import { getCellIds } from "../../../../chessBoard/gridCellIds.js";
+import { pieceState } from "../../../pieceState.js";
+import { gridContainer } from "../../../../chessBoard/objects/chessBoardElements.js";
+
+class PawnMoves {
+  
+}
